41eff815c4a438efc58928654dff60266981d68d
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2011-11-18 08:28:54 -0500

    Doesnt report an error on reset wizard

--- reset_wizard.sh
+++ reset_wizard.sh
@@ -2,6 +2,6 @@
 
 rm -f /etc/pf-xivo/web-interface/xivo.ini
 /etc/init.d/postgresql restart
-su postgres -c 'psql -c "drop database asterisk;"'
-su postgres -c 'psql -c "drop database xivo;"'
+su postgres -c 'psql -c "drop database if exists asterisk;"'
+su postgres -c 'psql -c "drop database if exists xivo;"'
 
