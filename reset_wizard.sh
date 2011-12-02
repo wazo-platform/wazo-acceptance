8249cc82ed14cca29a2e99afb88d880e3ba60596
Cedric Abunar <cabunar@avencall.com>
2011-12-02 11:18:49 -0500

    remove drop database in reset_wizard

--- reset_wizard.sh
+++ reset_wizard.sh
@@ -2,6 +2,4 @@
 
 rm -f /etc/pf-xivo/web-interface/xivo.ini
 /etc/init.d/postgresql restart
-su postgres -c 'psql -c "drop database if exists asterisk;"'
-su postgres -c 'psql -c "drop database if exists xivo;"'
 
--- web-interface/features/step_definitions/wizard_steps.py
+++ web-interface/features/step_definitions/wizard_steps.py
@@ -4,39 +4,48 @@ from lettuce import step
 from lettuce import world
 from features.step_definitions.login_steps import waitForLoginPage
 
+
 @step(u'Given that there is a XiVO installed at (.*)')
 def given_that_there_is_a_xivo_installed_at(step, ip):
     world.ip = ip
+
 
 @step(u'When I open the url')
 def when_i_open_the_url(step):
     world.browser.get(world.ip)
 
+
 @step(u'When I start the wizard')
 def when_i_start_the_wizard(step):
     world.browser.get(world.ip)
 
+
 @step(u'Then I should see the welcome message (.*)')
 def then_i_see_the_welcome_message(step, message):
     assert message in world.browser.page_source
+
 
 @step(u'When I select language (.*)')
 def when_i_select(step, language):
     language_option = world.browser.find_element_by_xpath('//option[@value="%s"]' % language)
     language_option.click()
 
+
 @step(u'When I click next')
 def when_i_click_next(step):
     world.browser.find_element_by_id('it-next').click()
+
 
 @step(u'When I click validate')
 def when_i_click_validate(step):
     world.browser.find_element_by_id('validate').click()
 
+
 @step(u'When I accept the terms of the licence')
 def when_i_accept_the_terms_of_the_licence(step):
     accept_box = world.browser.find_element_by_id('it-license-agree')
     accept_box.click()
+
 
 @step(u'Then I should be on the (.*) page')
 def then_i_should_be_on_page(step, page):
@@ -44,6 +53,7 @@ def then_i_should_be_on_page(step, page):
     world.browser.find_element_by_id(divid, '%s page not loaded' % (page,))
     div = world.browser.find_element_by_id(divid)
     assert div is not None
+
 
 @step(u'When I fill hostname (.*), domain (.*), password (.*) in the configuration page')
 def when_i_fill_the_configuration_page(step, hostname, domain, password):
@@ -56,6 +66,7 @@ def when_i_fill_the_configuration_page(step, hostname, domain, password):
     input_password.send_keys(password)
     input_password_confirm.send_keys(password)
 
+
 @step(u'When I fill entity (.*), start (.*), end (.*)')
 def when_i_fill_the_entity_context_page(step, entity, start, end):
     input_entity = world.browser.find_element_by_id('it-entity-displayname')
@@ -64,6 +75,7 @@ def when_i_fill_the_entity_context_page(step, entity, start, end):
     input_entity.send_keys(entity)
     input_start.send_keys(start)
     input_end.send_keys(end)
+
 
 @step(u'Then I should be redirected to the login page')
 def then_i_should_be_redirected_to_the_login_page(step):
