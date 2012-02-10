123b4ce4b6c461479d079b8b8ddcf21eaf02a26d
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-02-10 14:35:45 -0500

    split test in many classes

--- post-install/test_post_install.py
+++ post-install/test_post_install.py
@@ -4,19 +4,27 @@ import os
 from pwd import getpwnam
 
 
-class TestPostInstall(unittest.TestCase):
+class TestVoiceMailEmpty(unittest.TestCase):
 
     ASTERISK_VM_PATH = '/var/spool/asterisk/voicemail'
-    ASTERISK_SOUND_PATH = '/usr/share/asterisk/sounds/en'
-    DAHDI_PATH = '/dev/dahdi'
 
     def test_voicemail_dir_empty(self):
         self.assertTrue(os.path.exists(self.ASTERISK_VM_PATH))
         self.assertEqual(len(os.listdir(self.ASTERISK_VM_PATH)), 0)
 
+
+class TestSoundsInstalled(unittest.TestCase):
+
+    ASTERISK_SOUND_PATH = '/usr/share/asterisk/sounds/en'
+
     def test_sounds_file_are_installed(self):
         self.assertTrue(os.path.exists(self.ASTERISK_SOUND_PATH))
         self.assertFalse(len(os.listdir(self.ASTERISK_SOUND_PATH)) == 0)
+
+
+class TestAsteriskOwnsDahdi(unittest.TestCase):
+
+    DAHDI_PATH = '/dev/dahdi'
 
     def test_asterisk_owns_dahdi_files(self):
         self.assertTrue(os.path.exists(self.DAHDI_PATH))
