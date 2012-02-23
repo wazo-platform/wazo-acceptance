a5d8ff2679a61cda8393c59144d3ec68a9f545db
Cedric Abunar <cabunar@avencall.com>
2012-02-23 10:05:51 -0500

    [evol] new test: Add a conference room

--- webi/features/meetme.feature
+++ webi/features/meetme.feature
@@ -1,5 +1,10 @@
 Feature: Meetme
 
+    Scenario: Add a conference room
+        Given I am logged in
+        When I create a conference room with name "red" with number "9000"
+        Then meetme "red" is displayed in the list
+
     Scenario: Add a conference room with max participants set to 0
         Given I am logged in
         When I create a conference room with name "blue" with number "9000" with max participants "0"
--- webi/features/step_definitions/meetme_steps.py
+++ webi/features/step_definitions/meetme_steps.py
@@ -8,6 +8,17 @@ from xivo_lettuce.manager import meetme_manager
 from xivo_lettuce.manager import context_manager
 
 
+@step(u'When I create a conference room with name "([^"]*)" with number "([^"]*)"')
+def when_i_create_a_conference_room_with_name_number(step, name, confno):
+    context_manager.check_context_number_in_interval('default', 'meetme', confno)
+    meetme_manager.delete_all_meetme()
+    open_url('meetme', 'add')
+    meetme_manager.type_name(name)
+    meetme_manager.type_context('default')
+    meetme_manager.type_confno(confno)
+    submit_form()
+
+
 @step(u'When I create a conference room with name "([^"]*)" with number "([^"]*)" with max participants "([^"]*)"')
 def when_i_create_a_conference_room_with_name_number_max_participants(step, name, confno, maxusers):
     context_manager.check_context_number_in_interval('default', 'meetme', confno)
