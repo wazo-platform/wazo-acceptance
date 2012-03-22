01173669418abfae296d06f5d1a7d253e4214d83
Cedric Abunar <cabunar@avencall.com>
2012-03-22 14:54:31 -0400

    [evol]test dhcpd_update files exist after install

--- post-install/test_post_install.py
+++ post-install/test_post_install.py
@@ -35,3 +35,12 @@ class TestAsteriskOwnsDahdi(unittest.TestCase):
         asterisk_ids = getpwnam('asterisk')[2:4]
         file_ids = os.stat(file)[4:6]
         return asterisk_ids == file_ids
+
+
+class TestDhcpdUpdate(unittest.TestCase):
+
+    DHCPD_UPDATE_DIR = '/etc/dhcp/dhcpd_update'
+
+    def test_have_dhcpd_update_files(self):
+        self.assertTrue(os.access(self.DHCPD_UPDATE_DIR, os.R_OK))
+        self.assertTrue(len(os.listdir(self.DHCPD_UPDATE_DIR)) > 0)
