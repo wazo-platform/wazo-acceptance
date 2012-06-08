899c4fd85eaf4ca73c26b391ddc28a22f789a4d4
Jean-Yves LEBLEU <jylebleu@avencall.com>
2012-06-08 14:36:36 -0400

    added incall edit webservice test

--- webservices/tests/test_incall_webservice.py
+++ webservices/tests/test_incall_webservice.py
@@ -26,15 +26,85 @@ from webservices.webservices import WebServicesFactory
 class TestIncall(unittest.TestCase):
     def setUp(self):
         self._aws = WebServices('ipbx/call_management/incall')
-        self._aws.deleteall()
+        self.context_name = "test_incall_context"
+        self._add_context()
 
     def tearDown(self):
         pass
 
+    def new_incall_content(self, exten):
+        return {
+            "incall": {
+                "exten": unicode(exten),
+                "context": unicode(self.context_name),
+                "preprocess_subroutine": "",
+            },
+            "dialaction": {
+                "answer": {
+				 "actiontype": "endcall:hangup",
+				 "actionarg1": "",
+				 "actionarg2": ""
+                }
+            }
+        }
+
+
     def test_add(self):
+        incall_number = "5000"
+        self._delete_incall(incall_number)
+        var_incall = self.new_incall_content(incall_number)
+
+        response = self._aws.add(var_incall)
+        self.assertEqual(response.code, 200)
+
+        response = self._aws.search(incall_number)
+        self.assertEqual(response.code, 200)
+
+    def test_delete(self):
+        incall_number = "5100"
+        incall_id = self._add_incall(incall_number)
+
+        response = self._aws.delete(incall_id)
+        self.assertEqual(response.code, 200)
+
+        response = self._aws.search(incall_number)
+        self.assertEqual(response.code, 204)
+
+    def assertIncallActionModified(self, incall_number, action):
+        response = self._aws.search(incall_number)
+        incalls = json.loads(response.data)
+        self.assertEquals(incalls[0]['action'], action, "action not modified")
+
+
+    def test_edit(self):
+        incall_number = "5050"
+        incall_id = self._add_incall(incall_number)
+        var_incall = self.new_incall_content(incall_number)
+        var_incall['dialaction']['answer']['actiontype'] = 'endcall:busy'
+        response = self._aws.edit(var_incall, incall_id)
+        self.assertEqual(response.code, 200)
+
+        self.assertIncallActionModified(incall_number, "endcall:busy")
+
+    def _delete_incall(self, incall_number):
+        response = self._aws.search(incall_number)
+        if response.code == 200:
+            incalls = json.loads(response.data)
+            self._aws.delete(incalls[0]['id'])
+
+    def _add_incall(self, incall_number):
+        self._delete_incall(incall_number)
+        var_incall = self.new_incall_content(incall_number)
+        self._aws.add(var_incall)
+        response = self._aws.search(incall_number)
+        incalls = json.loads(response.data)
+        return incalls[0]['id']
+
+    def _add_context(self):
+
         var_context = {
-                      "name": 'toto',
-                      "displayname" : 'Toto Corp.',
+                      "name": unicode(self.context_name),
+                      "displayname" : "Display %s" % self.context_name,
                       "entity" : 'avencall',
                       "contexttype": 'incall',
                       "contextinclude": '[]',
@@ -48,35 +118,5 @@ class TestIncall(unittest.TestCase):
         jsonstr = jsonfilecontent % (var_context)
         data = json.loads(jsonstr)
         wsctx = WebServicesFactory('ipbx/system_management/context')
-        wsctx.delete('toto')
+        wsctx.delete(self.context_name)
         wsctx.add(data)
-
-        var_incall = {
-                      "exten": '5000',
-                      "context" : var_context['name']
-                      }
-        jsonfilecontent = self._aws.get_json_file_content('incall');
-        jsonstr = jsonfilecontent % (var_incall)
-        content = json.loads(jsonstr)
-
-        response = self._aws.add(content)
-        self.assertEqual(response.code, 200)
-
-        response = self._aws.search(var_incall['exten'])
-        self.assertEqual(response.code, 200)
-        wsctx.delete(var_context['name'])
-        res = json.loads(response.data)
-        self.assertEqual(len(res), 1)
-        if 'id' in res[0]:
-            return res[0]['id']
-
-    def test_delete(self):
-        id = self.test_add()
-        response = self._aws.delete(id)
-        self.assertEqual(response.code, 200)
-        response = self._aws.list()
-        self.assertEqual(response.code, 204)
-
-
-if __name__ == '__main__':
-    unittest.main()
