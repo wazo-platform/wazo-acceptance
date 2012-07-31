0659c334be3c7dd9868b7cf151ff8742dfadac74
Cedric Abunar <cabunar@avencall.com>
2012-07-31 15:01:45 -0400

    [fix]rename python reserved package name (stat to statcenter)

---
+++ statcenter/alltests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# sh -c is mandatory to have basename working after the substitution of {}
+find features/ -name '*.feature' -exec sh -c 'PYTHONPATH=.. lettuce --with-xunit --verbosity=4 {} --xunit-file=${PWD}/$(basename {} .feature).xml' \;
+
