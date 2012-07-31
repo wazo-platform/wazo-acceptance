65393a6de82560948d6860a126b946ba81be8c27
Cedric Abunar <cabunar@avencall.com>
2012-07-31 15:09:46 -0400

    [fix]set versbosity level to 3 for statcenter feature

--- statcenter/alltests.sh
+++ statcenter/alltests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # sh -c is mandatory to have basename working after the substitution of {}
-find features/ -name '*.feature' -exec sh -c 'PYTHONPATH=.. lettuce --with-xunit --verbosity=4 {} --xunit-file=${PWD}/$(basename {} .feature).xml' \;
+find features/ -name '*.feature' -exec sh -c 'PYTHONPATH=.. lettuce --with-xunit --verbosity=3 {} --xunit-file=${PWD}/$(basename {} .feature).xml' \;
 
