f04faf3da2748ad91680a701b5b5af7998e4cf16
Etienne Lessard <elessard@avencall.com>
2012-08-06 08:51:00 -0400

    remove empty context_steps file.

--- webi/features/step_definitions/context_steps.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import time
-
-from lettuce.decorators import step
-from lettuce.registry import world
-
-from xivo_lettuce.common import *
-from xivo_lettuce.manager.context_manager import *
