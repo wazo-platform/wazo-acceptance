76752179afe9b58522ab658451abac28b7d89b3e
Cedric Abunar <cabunar@avencall.com>
2012-09-07 11:22:44 -0400

    [fix]update import for cticlient test

--- cticlient/features/step_definitions/__init__.py
+++ cticlient/features/step_definitions/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from xivo_lettuce import terrain
-from xivo_lettuce.common_steps import *
-from webi.features.step_definitions import connection_steps, user_steps, agent_steps
+from xivo_lettuce.common_steps_webi import *
+from xivo_lettuce.common_steps_callgen import *
+from webi.features.step_definitions import user_steps, agent_steps
