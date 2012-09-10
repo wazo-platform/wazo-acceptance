9d54573f34daa7567fe65cf012e7d47ebd4db72e
Cedric Abunar <cabunar@avencall.com>
2012-09-10 14:26:30 -0400

    [evo]automated test X-371

--- post-install/test_post_install.py
+++ post-install/test_post_install.py
@@ -2,6 +2,9 @@
 
 import unittest
 import os
+import subprocess
+import pwd
+import grp
 
 from pwd import getpwnam
 
@@ -37,3 +40,42 @@ class TestAsteriskOwnsDahdi(unittest.TestCase):
         asterisk_ids = getpwnam('asterisk')[2:4]
         file_ids = os.stat(file)[4:6]
         return asterisk_ids == file_ids
+
+
+class TestMohFilesPermission(unittest.TestCase):
+
+    MOH_PATH = '/usr/share/asterisk/moh/default'
+    MOH_PKG = 'asterisk-moh-opsound-wav'
+    FILE_UID = 'asterisk'
+    FILE_GID = 'www-data'
+
+    def test_files_are_installed(self):
+        command = ['apt-get', 'install', self.MOH_PKG]
+
+        p = subprocess.Popen(command,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             close_fds=True)
+
+        (stdoutdata, stderrdata) = p.communicate()
+
+        if p.returncode != 0:
+            print stderrdata
+
+    def test_files_owned_by_asterisk(self):
+        files = os.listdir(self.MOH_PATH)
+
+        for file in files:
+            file_path = os.path.join(self.MOH_PATH, file)
+            uid = self._find_uid(file_path)
+            gid = self._find_gid(file_path)
+            self.assertEqual(self.FILE_UID, uid, 'File %s : UID %s not owned by %s ' % (file, uid, self.FILE_UID))
+            self.assertEqual(self.FILE_GID, gid, 'File %s : GID %s not owned by %s ' % (file, gid, self.FILE_GID))
+
+
+    def _find_uid(self, file):
+        return pwd.getpwuid(os.stat(file).st_uid).pw_name
+
+    def _find_gid(self, file):
+        return grp.getgrgid(os.stat(file).st_gid).gr_name
