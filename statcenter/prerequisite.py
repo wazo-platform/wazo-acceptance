887bf9a50ebea2e806138ed1f14836c1053dd5e2
Sebastien Duthil <sduthil@avencall.com>
2012-09-19 14:21:31 -0400

    [evol] improve statcenter prerequisites

--- statcenter/prerequisite.py
+++ statcenter/prerequisite.py
@@ -1,29 +1,55 @@
 # -*- coding: UTF-8 -*-
 
 import socket
+from subprocess import Popen
 from lettuce import world
 from xivo_lettuce.manager_ws import context_manager_ws, trunksip_manager_ws
 from xivo_lettuce.terrain import initialize_from_config
 
 
+
 def main():
+    print 'Initializing ...'
     initialize_from_config()
+    print 'Adding queue extensions interval'
     context_manager_ws.update_contextnumbers_queue('statscenter', 5000, 5100)
+    print 'Adding user extensions interval'
     context_manager_ws.update_contextnumbers_user('statscenter', 1000, 1100)
 
     callgen_ip = socket.gethostbyname(world.callgen_host)
+    print 'Adding default SIP trunk'
     trunksip_manager_ws.add_or_replace_trunksip(callgen_ip, 'to_default', 'default')
+    print 'Adding statscenter SIP trunk'
     trunksip_manager_ws.add_or_replace_trunksip(callgen_ip, 'to_statscenter', 'statscenter')
 
-    _create_pgpass_on_remote_host(world.ssh_client_xivo)
+    print 'Configuring PostgreSQL on XiVO'
+    _create_pgpass_on_remote_host()
+    _allow_remote_access_to_pgsql()
 
 
-def _create_pgpass_on_remote_host(ssh_xivo):
+def _create_pgpass_on_remote_host():
     cmd = ['echo', '*:*:asterisk:asterisk:proformatique', '>', '.pgpass']
-    ssh_xivo.check_call(cmd)
+    world.ssh_client_xivo.check_call(cmd)
     cmd = ['chmod', '600', '.pgpass']
-    ssh_xivo.check_call(cmd)
+    world.ssh_client_xivo.check_call(cmd)
 
+def _allow_remote_access_to_pgsql():
+    hba_file = '/etc/postgresql/9.0/main/pg_hba.conf'
+    postgres_conf_file = '/etc/postgresql/9.0/main/postgresql.conf'
+
+    _add_line_to_remote_file('host all all 192.168.32.0/24 md5', hba_file)
+    _add_line_to_remote_file('host all all 10.0.0.0/8 md5', hba_file)
+    _add_line_to_remote_file("listen_addresses = '*'", postgres_conf_file)
+
+    _restart_postgres()
+
+def _add_line_to_remote_file(line_text, file_name):
+    command = ['grep', '192.168.32.0/24', file_name, '||', '$(echo "%s" >> %s)' % (line_text, file_name)]
+    world.ssh_client_xivo.check_call(command)
+
+def _restart_postgres():
+    command = ['service', 'postgresql', 'restart']
+    world.ssh_client_xivo.check_call(command)
 
 if __name__ == '__main__':
     main()
