9a762d092dac4a74fa836fbd1cd195df78e9ddf8
Sebastien Duthil <sduthil@avencall.com>
2012-09-25 15:54:24 -0400

    [fix] wrong escaping of command

--- statcenter/prerequisite.py
+++ statcenter/prerequisite.py
@@ -47,7 +47,7 @@ def _allow_remote_access_to_pgsql():
 
 
 def _add_line_to_remote_file(line_text, file_name):
-    command = ['grep "%s" %s' % (line_text, file_name), '||', '$(echo "%s" >> %s)' % (line_text, file_name)]
+    command = ['grep', '-F', '"%s"' % line_text, file_name, '||', '$(echo "%s" >> %s)' % (line_text, file_name)]
     world.ssh_client_xivo.check_call(command)
 
 
