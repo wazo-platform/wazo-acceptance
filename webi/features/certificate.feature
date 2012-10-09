e7be0aa8666f61e9f6a4f49c5298809c6bab3112
Gregory Eric Sanderson <gsanderson@avencall.com>
2012-10-09 15:19:01 -0400

    refactor certificate steps
     - Rewrite form test, make it more readable
     - Automate test X-89, valid certificate in asterisk

--- webi/features/certificate.feature
+++ webi/features/certificate.feature
@@ -1,9 +1,25 @@
 Feature: Certificates
 
     Scenario: Create a certificate with invalid info
-        Given there are no certificates named "Cert"
-        When I create a certificate with name "Cert" and with email "test@test@test.com" and validity date "10/18/2014"
-        When I submit with errors
-        When I create a certificate with name "Cert" and with email "test@test.com" and validity date "10/18/2014"
-        When I submit
+        When I create a certificate with the following invalid info:
+          | name | email              | valid date in the future |
+          | Cert | test@test@test.com | yes                      |
+        Then I see errors
+        When I create a certificate with following valid info:
+          | name | email         | valid date in the future |
+          | Cert | test@test.com | yes                      |
         Then I see no errors
+
+    Scenario: Create a certificate with valid info
+        Given I create a certificate with following valid info and the server's hostname as common name:
+          | name      | email           | valid date in the future | certificate authority | autosigned |
+          | phones    | foo@example.org | yes                      | no                    | yes        |
+          | phones-ca | foo@example.org | yes                      | yes                   | yes        |
+
+        When I enable the following options for the SIP Protocol:
+          | allow tls connections | listening address | server certificate | ca certificate |
+          | yes                   | 0.0.0.0           | phones             | phones-ca      |
+
+        Then SIP tls connections use the "phones" certificate for encryption
+        Then there are no warnings when reloading sip configuration
+
--- webi/features/step_definitions/certificate_steps.py
+++ webi/features/step_definitions/certificate_steps.py
@@ -1,26 +1,114 @@
 # -*- coding: utf-8 -*-
+import datetime
 
 from lettuce import step, world
-from xivo_lettuce.common import open_url, remove_all_elements
+from xivo_lettuce import form, logs
+from xivo_lettuce.common import open_url, remove_all_elements, go_to_tab
+from xivo_lettuce.manager import asterisk_manager
+from xivo_lettuce.checkbox import Checkbox
+from selenium.webdriver.support.select import Select
 
 
-def create_basic_certificate(name, email, validity_dt):
+def create_or_replace_certificate(info):
+    remove_all_elements('certificat', info['name'])
+
     open_url('certificat', 'add')
+
     input_name = world.browser.find_element_by_id('it-name')
-    input_name.send_keys(name)
-    autosign_check = world.browser.find_element_by_id('it-autosigned')
-    autosign_check.click()
-    input_date = world.browser.find_element_by_id('it-validity-end')
-    input_date.send_keys(validity_dt)
+    input_name.send_keys(info['name'])
+
     input_email = world.browser.find_element_by_id('it-subject-emailAddress')
-    input_email.send_keys(email)
+    input_email.clear()
+    input_email.send_keys(info['email'])
+
+    input_date = world.browser.find_element_by_id('it-validity-end')
+    input_date.clear()
+
+    date = datetime.datetime.now()
+    if info['valid date in the future'] == "yes":
+        date += datetime.timedelta(days=31)
+    else:
+        date -= datetime.timedelta(days=1)
+
+    input_date.send_keys(date.strftime("%m/%d/%Y"))
+
+    if 'autosigned' in info:
+        autosign_check = world.browser.find_element_by_id('it-autosigned')
+        checked = info['autosigned'] == "yes"
+        Checkbox(autosign_check).set_checked(checked)
+
+    if 'certificate authority' in info:
+        authority_check = world.browser.find_element_by_id('it-is_ca')
+        checked = info['certificate authority'] == "yes"
+        Checkbox(authority_check).set_checked(checked)
 
 
-@step(u'Given there are no certificates named "([^"]*)"')
-def given_there_are_no_certificats_named_group1(step, name):
-    remove_all_elements('certificat', name)
+
+def insert_hostname_into_form():
+    command = ['hostname', '-f']
+    hostname = world.ssh_client_xivo.out_call(command).strip()
+
+    input_cn = world.browser.find_element_by_id("it-subject-CN")
+    input_cn.send_keys(hostname)
+
+def update_sip_configuration(info):
+    open_url('general_sip')
+    go_to_tab('Security')
+
+    checked = info['allow tls connections'] == "yes"
+    Checkbox.from_label("Allow TLS connections").set_checked(checked)
+
+    form.set_text_field("Listening address", info['listening address'])
+
+    form.set_select_field("Server certificate", info['server certificate'])
+    form.set_select_field("CA certificate", info['ca certificate'])
 
 
-@step(u'When I create a certificate with name "([^"]*)" and with email "([^"]*)" and validity date "([^"]*)"')
-def when_i_create_a_certificat_with_name_group1_and_with_email_group2_and_validity_date_group3(step, name, email, validity_dt):
-    create_basic_certificate(name, email, validity_dt)
+@step(u'When I create a certificate with the following invalid info:')
+def when_i_create_a_certificate_with_the_following_invalid_info(step):
+    for info in step.hashes:
+        create_or_replace_certificate(info)
+        form.submit_form_with_errors()
+
+@step(u'When I create a certificate with following valid info:')
+def when_i_create_a_certificate_with_following_valid_info(step):
+    for info in step.hashes:
+        create_or_replace_certificate(info)
+        form.submit_form()
+
+
+@step(u'Given I create a certificate with following valid info and the server\'s hostname as common name:')
+def given_i_create_a_certificate_with_following_valid_info_and_the_server_s_hostname_as_common_name(step):
+    for info in step.hashes:
+        create_or_replace_certificate(info)
+        insert_hostname_into_form()
+        form.submit_form()
+
+@step(u'When I enable the following options for the SIP Protocol:')
+def when_i_enable_the_following_options_for_the_sip_protocol(step):
+    for info in step.hashes:
+        update_sip_configuration(info)
+
+
+@step(u'Then SIP tls connections use the "([^"]*)" certificate for encryption')
+def then_sip_tls_connections_use_the_group1_certificate_for_encryption(step, certificate):
+    configfile = "sip.conf"
+    certificate_path = "/var/lib/pf-xivo/certificates/%s.pem" % certificate
+    variable = "tlscertfile"
+    current_path = asterisk_manager.get_asterisk_conf(configfile, variable)
+    assert(current_path == certificate_path)
+
+
+@step(u'Then there are no warnings when reloading sip configuration')
+def then_there_are_no_warnings_when_reloading_sip_configuration(step):
+
+    now = datetime.datetime.now()
+
+    asterisk_manager.send_to_asterisk_cli("sip reload")
+    command = ['tac', '/var/log/asterisk/messages', '|', 'grep', 'WARNING', '--max-count', '5']
+    output = world.ssh_client_xivo.out_call(command).strip()
+
+    lines = logs.read_last_log_lines(output, now)
+    assert len(lines) > 1
+
+
