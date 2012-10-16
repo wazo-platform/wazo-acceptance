dd07ea25c6be1ddf5729b64cb37c453d29f455b4
Etienne Lessard <elessard@avencall.com>
2012-10-16 15:38:18 -0400

    remove subcommand from loadtester
    There was only one remaining subcommand, "start".

--- load-tester/loadtester/bin/loadtester.py
+++ load-tester/loadtester/bin/loadtester.py
@@ -2,62 +2,49 @@
 
 from __future__ import absolute_import
 
+import argparse
 import logging
 import os.path
-from loadtester.bin import commands
 from loadtester.config import ScenarioConfig
 from loadtester.runner import ScenarioRunner
 from loadtester.scenario import Scenario
 
+_CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../etc'))
+_CONFIG_FILE = os.path.join(_CONFIG_DIR, 'conf.py')
+
 
 def main():
     _init_logging()
-    commands.execute_command(_LoadtesterCommand())
+
+    parsed_args = _parse_args()
+
+    if parsed_args.verbose:
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+    scenario_runner = ScenarioRunner()
+    scenario = Scenario(parsed_args.scenario_dir)
+    scenario_config = ScenarioConfig.new_from_filename(parsed_args.conf)
+    scenario_runner.start_scenario(scenario, scenario_config)
 
 
 def _init_logging():
     logger = logging.getLogger()
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter('%(levelname)s:%(message)s'))
+    handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
     logger.addHandler(handler)
     logger.setLevel(logging.WARNING)
 
 
-class _LoadtesterCommand(commands.AbstractCommand):
-    def configure_parser(self, parser):
-        parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                            help='increase logging verbosity')
-
-    def configure_subcommands(self, subcommands):
-        subcommands.add_subcommand(_StartSubcommand('start'))
-
-    def pre_execute(self, parsed_args):
-        self._set_logging_verbosity(parsed_args)
-        self._set_scenario_runner(parsed_args)
-
-    def _set_logging_verbosity(self, parsed_args):
-        if parsed_args.verbose:
-            logger = logging.getLogger()
-            logger.setLevel(logging.INFO)
-
-    def _set_scenario_runner(self, parsed_args):
-        parsed_args.scenario_runner = ScenarioRunner()
-
-
-class _StartSubcommand(commands.AbstractSubcommand):
-    _CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../etc'))
-    _CONFIG_FILE = os.path.join(_CONFIG_DIR, 'conf.py')
-
-    def configure_parser(self, parser):
-        parser.add_argument('-c', '--conf', default=self._CONFIG_FILE,
-                            help='path to the config file')
-        parser.add_argument('scenario_dir',
-                            help='path to the scenario directory')
-
-    def execute(self, parsed_args):
-        scenario = Scenario(parsed_args.scenario_dir)
-        scenario_config = ScenarioConfig.new_from_filename(parsed_args.conf)
-        parsed_args.scenario_runner.start_scenario(scenario, scenario_config)
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='increase verbosity')
+    parser.add_argument('-c', '--conf', default=_CONFIG_FILE,
+                        help='path to the config file')
+    parser.add_argument('scenario_dir',
+                        help='path to the scenario directory')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
