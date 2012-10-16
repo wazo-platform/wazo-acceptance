736507355ff86e651515af15d307d961f6141516
Cedric Abunar <cabunar@avencall.com>
2012-10-16 08:44:03 -0400

    [fix]pep8 warnings

--- webi/features/step_definitions/general_settings_sccp_steps.py
+++ webi/features/step_definitions/general_settings_sccp_steps.py
@@ -2,40 +2,47 @@
 
 from lettuce import step, world
 from xivo_lettuce import form
-from xivo_lettuce.checkbox import Checkbox
-from xivo_lettuce.common import go_to_tab, open_url
+from xivo_lettuce.common import open_url
 from xivo_lettuce.manager import asterisk_manager
 from xivo_lettuce.manager_ws import sccp_general_settings_manager_ws as sccp_manager
 from selenium.webdriver.support.select import Select
+
 
 @step('Given the directmedia option is disabled')
 def given_the_directmedia_option_is_disabled(step):
     sccp_manager.disable_directmedia()
 
+
 @step('Given the directmedia option is enabled')
 def given_the_directmedia_option_is_enabled(step):
     sccp_manager.enable_directmedia()
+
 
 @step('Given the dial timeout is at (\d+) seconds')
 def given_the_dial_timeout_is_at_x_seconds(step, timeout):
     sccp_manager.set_dialtimeout(timeout)
 
+
 @step('Given the language option is at "([^"]*)"')
-def given_the_dial_timeout_is_at_x_seconds(step, language):
+def given_the_language_option_is_at(step, language):
     sccp_manager.set_language(language)
+
 
 @step('Given I am on the SCCP General Settings page')
 def given_i_am_on_the_sccp_general_settings_page(step):
     open_url('sccpgeneralsettings')
+
 
 @step('When I click the directmedia checkbox')
 def when_i_click_the_directmedia_checkbox(step):
     directmedia_checkbox = world.browser.find_element_by_id("it-sccpgeneralsettings-directmedia")
     directmedia_checkbox.click()
 
+
 @step('When I submit the form')
 def when_i_submit_the_form(step):
     form.submit_form()
+
 
 @step('When I change the dial timeout to "([^"]*)"')
 def when_i_change_the_dial_timeout(step, timeout):
@@ -48,6 +55,7 @@ def when_i_change_the_dial_timeout(step, timeout):
 def when_i_select_the_language(step, language):
     language_dropdown = Select(world.browser.find_element_by_id('it-sccpgeneralsettings-language'))
     language_dropdown.select_by_visible_text(language)
+
 
 @step('Then the option "([^"]*)" is at "([^"]*)" in sccp.conf')
 def then_the_option_is_at_x_in_sccp_conf(step, option, expected_value):
