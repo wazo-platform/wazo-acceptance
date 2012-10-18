71c07a75b22fe386c161e807581d8621ccefd733
Etienne Lessard <elessard@avencall.com>
2012-10-18 11:14:14 -0400

    missed renaming in user_import_manager_ws

--- xivo_lettuce/manager_ws/user_import_manager_ws.py
+++ xivo_lettuce/manager_ws/user_import_manager_ws.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from lettuce import world
-from xivo_lettuce.manager_ws.voicemail_manager_ws import delete_voicemail_with_number
+from xivo_lettuce.manager_ws.voicemail_manager_ws import delete_all_voicemails_with_number
 from xivo_lettuce.manager_ws.user_manager_ws import delete_user_with_firstname_lastname
 from xivo_lettuce.manager_ws.line_manager_ws import delete_line_with_number
 from xivo_lettuce.manager_ws.incall_manager_ws import delete_incall_with_did
@@ -11,7 +11,7 @@ from xivo_ws.objects.user import User, UserLine, UserVoicemail, UserIncall
 def insert_simple_user(entries):
     users = list()
     for entry in entries:
-        delete_voicemail_with_number(entry['linenumber'])
+        delete_all_voicemails_with_number(entry['linenumber'])
         delete_line_with_number(entry['linenumber'], entry['context'])
         delete_user_with_firstname_lastname(entry['firstname'], entry['lastname'])
         user = User()
@@ -28,7 +28,7 @@ def insert_simple_user(entries):
 def insert_adv_user_with_mevo(entries):
     users = list()
     for entry in entries:
-        delete_voicemail_with_number(entry['linenumber'])
+        delete_all_voicemails_with_number(entry['linenumber'])
         delete_line_with_number(entry['linenumber'], entry['context'])
         delete_user_with_firstname_lastname(entry['firstname'], entry['lastname'])
         user = User()
@@ -49,7 +49,7 @@ def insert_adv_user_with_mevo(entries):
 def insert_adv_user_with_incall(entries):
     users = list()
     for entry in entries:
-        delete_voicemail_with_number(entry['linenumber'])
+        delete_all_voicemails_with_number(entry['linenumber'])
         delete_line_with_number(entry['linenumber'], entry['context'])
         delete_user_with_firstname_lastname(entry['firstname'], entry['lastname'])
         delete_incall_with_did(entry['incall'])
@@ -70,7 +70,7 @@ def insert_adv_user_with_incall(entries):
 def insert_adv_user_full_infos(entries):
     users = list()
     for entry in entries:
-        delete_voicemail_with_number(entry['linenumber'])
+        delete_all_voicemails_with_number(entry['linenumber'])
         delete_line_with_number(entry['linenumber'], entry['context'])
         delete_user_with_firstname_lastname(entry['firstname'], entry['lastname'])
         delete_incall_with_did(entry['incall'])
