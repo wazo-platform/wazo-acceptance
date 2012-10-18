061be34842e8df21e586b953e672b948942df064
Cedric Abunar <cabunar@avencall.com>
2012-10-18 11:14:21 -0400

    [refactor]merge prerequisites files

--- utils/prerequisite.py
+++ utils/prerequisite.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import socket
+
 from lettuce import world
 from xivo_lettuce.manager_ws import context_manager_ws, trunksip_manager_ws
 from xivo_lettuce.terrain import initialize, deinitialize
@@ -10,20 +11,30 @@ def main():
     print 'Initializing ...'
     initialize()
     try:
-        print 'Adding queue extensions interval'
-        context_manager_ws.update_contextnumbers_queue('statscenter', 5000, 5100)
-        print 'Adding user extensions interval'
-        context_manager_ws.update_contextnumbers_user('statscenter', 1000, 1100)
-
         callgen_ip = socket.gethostbyname(world.callgen_host)
+
+        print 'Adding context'
+        context_manager_ws.update_contextnumbers_queue('statscenter', 5000, 5100)
+        context_manager_ws.update_contextnumbers_user('statscenter', 1000, 1100)
+        context_manager_ws.update_contextnumbers_user('default', 1000, 1999)
+        context_manager_ws.update_contextnumbers_group('default', 2000, 2999)
+        context_manager_ws.update_contextnumbers_queue('default', 3000, 3999)
+        context_manager_ws.update_contextnumbers_meetme('default', 4000, 4999)
+        context_manager_ws.update_contextnumbers_incall('from-extern', 1000, 4999, 4)
+
         print 'Adding default SIP trunk'
         trunksip_manager_ws.add_or_replace_trunksip(callgen_ip, 'to_default', 'default')
+
         print 'Adding statscenter SIP trunk'
         trunksip_manager_ws.add_or_replace_trunksip(callgen_ip, 'to_statscenter', 'statscenter')
+
+        print 'Adding default SIP trunk'
+        trunksip_manager_ws.add_or_replace_trunksip(callgen_ip, 'to_incall', 'from-extern')
 
         print 'Configuring PostgreSQL on XiVO'
         _create_pgpass_on_remote_host()
         _allow_remote_access_to_pgsql()
+
     finally:
         deinitialize()
 
