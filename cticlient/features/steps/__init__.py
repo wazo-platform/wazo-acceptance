44aa1a08f8dbbf706dc96817de310bd733545371
Cedric Abunar <cabunar@avencall.com>
2012-10-18 10:29:37 -0400

    [refactor]add new package xivo_configuration

--- cticlient/features/steps/__init__.py
+++ cticlient/features/steps/__init__.py
@@ -4,6 +4,6 @@ from xivo_lettuce import terrain
 import xivo_lettuce.common_steps_webi
 import xivo_lettuce.common_steps_callgen
 import xivo_lettuce.common_steps_xivoclient
-from webi.features.steps import user_steps, sheet_steps, \
-     incall_steps, group_steps
-from webi_call_center.features.steps import agent_steps, queue_steps
+from webi.features.steps import user_steps, incall_steps, group_steps
+from cti_server.features.steps import sheet_steps
+from call_center.features.steps import agent_steps, queue_steps
--- statcenter/features/steps/__init__.py
+++ statcenter/features/steps/__init__.py
@@ -4,4 +4,4 @@ from xivo_lettuce import terrain
 from xivo_lettuce.common_steps_webi import *
 from xivo_lettuce.common_steps_callgen import *
 from webi.features.steps import user_steps
-from webi_call_center.features.steps import agent_steps, queue_steps
+from call_center.features.steps import agent_steps, queue_steps
