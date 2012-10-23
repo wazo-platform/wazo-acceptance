305e092b259fc4c5af6f0483c57519d403fef116
Sebastien Duthil <sduthil@avencall.com>
2012-10-23 08:32:11 -0400

    move cticlient to features/cti_client

--- cticlient/alltests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# sh -c is mandatory to have basename working after the substitution of {}
-find features/ -name '*.feature' -exec sh -c 'PYTHONPATH=.. lettuce --with-xunit --verbosity=3 {} --xunit-file=$PWD/$(basename {} .feature).xml' \;
