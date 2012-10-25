ada8ac5a3d30abaa8dab120880a5268220e36ffa
Cedric Abunar <cabunar@avencall.com>
2012-10-25 08:52:35 -0400

    [refactor]move xivo_configuration and webi to features

--- features/call_generator/steps/__init__.py
+++ features/call_generator/steps/__init__.py
@@ -3,5 +3,5 @@
 from xivo_lettuce import terrain
 from xivo_lettuce.common_steps_webi import *
 from xivo_lettuce.common_steps_callgen import *
-from webi.features.steps import user_steps, group_steps
+from features.webi.steps import user_steps, group_steps
 from features.call_center.steps import queue_steps, agent_steps
--- features/cti_client/steps/__init__.py
+++ features/cti_client/steps/__init__.py
@@ -4,6 +4,6 @@ from xivo_lettuce import terrain
 import xivo_lettuce.common_steps_webi
 import xivo_lettuce.common_steps_callgen
 import xivo_lettuce.common_steps_xivoclient
-from webi.features.steps import user_steps, incall_steps, group_steps
+from features.webi.steps import user_steps, incall_steps, group_steps
 from features.cti_server.steps import sheet_steps
 from features.call_center.steps import agent_steps, queue_steps, supervisor_steps
