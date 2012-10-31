0a94630b98348fc2094693e8a86b9b920d0a14b1
Sebastien Duthil <sduthil@avencall.com>
2012-10-31 16:57:38 -0400

    refactor testlink script

--- utils/testlink.sh
+++ utils/testlink.sh
@@ -5,18 +5,41 @@ function usage {
 }
 
 echo "See https://wiki.xivo.fr/index.php/JenkinsAndTestlink for instructions about Testlink."
-echo "Here are the classnames, sorted from the more recently edited to the most ancient."
+echo "Here are the classnames, sorted from the more recently edited to the oldest."
 echo "You can filter automated tests in Testlink to see if some tests are missing from Testlink."
 echo
+
+function features_path {
+    this_script_directory="$(dirname $0)"
+    relative_path_to_features=".."
+    echo "$this_script_directory/$relative_path_to_features"
+}
 
 function add_git_date {
     while read filename
     do
-        git log -1 --format=%ct $filename | sed "s|\$|:$filename|"
+        git_date="$(git log -1 --format=%ct $filename)"
+        echo "$git_date:$filename"
     done
 }
 
-function extract_features {
+function sort_numeric {
+    sort -g <&0
+}
+
+function remove_git_date {
+    cut -d: -f2 <&0
+}
+
+function list_feature_files {
+    find . -name '*.feature' -print
+}
+
+function sort_by_git_edit_date {
+    add_git_date <&0 | sort_numeric | remove_git_date
+}
+
+function extract_classnames {
     while read filename
     do
         awk \
@@ -29,20 +52,13 @@ BEGIN {FS = ": "}
     done
 }
 
-# Get filenames
-find .. -name '*.feature' | \
+function append_line_count {
+    tee <&0 >(wc -l | awk '{print "\nTotal : " $0}')
+}
 
-# Add sort indicator
-add_git_date | \
+function list_classnames {
+    list_feature_files | sort_by_git_edit_date | extract_classnames | append_line_count
+}
 
-# Sort
-sort -rg | \
-
-# Remove sort indicator
-cut -d: -f2 | \
-
-# Print feature : scenario
-extract_features | \
-
-# Count lines and print
-tee >(wc -l | awk '{print "\nTotal : " $0}')
+cd "$(features_path)"
+list_classnames
