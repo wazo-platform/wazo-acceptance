2e177f3b2f35b0e4e1268d5b57a3da101f4db1c5
Etienne Lessard <elessard@avencall.com>
2012-11-02 08:48:30 -0400

    "given there is an incall" step now use the ws instead of the webi

--- features/webi/steps/incall_steps.py
+++ features/webi/steps/incall_steps.py
@@ -8,28 +8,24 @@ from xivo_lettuce.manager import incall_manager as incall_man
 from xivo_lettuce.manager_ws import incall_manager_ws
 
 
-@step(u'When I create an incall with DID "([^"]*)" in context "([^"]*)"')
-def when_i_create_incall_with_did(step, incall_did, context):
-    open_url('incall', 'add')
-    incall_man.type_incall_did(incall_did)
-    incall_man.type_incall_context(context)
-    form.submit_form()
-
-
 @step(u'Given there is an incall "([^"]*)" in context "([^"]*)" to the "([^"]*)" "([^"]*)"$')
 def given_there_is_an_incall_group1_in_context_group2_to(step, did, context, dst_type, dst_name):
     incall_manager_ws.delete_incalls_with_did(did)
     incall_manager_ws.add_incall(did, context, dst_type, dst_name)
 
 
-@step(u'Given there is an incall "([^"]*)" in context "([^"]*)" to the "(.+)" "([^"]*)" with caller id name "([^"]*)" number "([^"]*)"')
-def given_there_is_an_incall_group1_in_context_group2_to_the_queue_group3(step, did, context, destination_type, destination_name, caller_id_name, caller_id_number):
+@step(u'Given there is an incall "([^"]*)" in context "([^"]*)" to the "([^"]*)" "([^"]*)" with caller id name "([^"]*)" number "([^"]*)"')
+def given_there_is_an_incall_group1_in_context_group2_to_the_queue_group3(step, did, context, dst_type, dst_name, cid_name, cid_num):
+    caller_id = '"%s" <%s>' % (cid_name, cid_num)
     incall_manager_ws.delete_incalls_with_did(did)
+    incall_manager_ws.add_incall(did, context, dst_type, dst_name, caller_id)
+
+
+@step(u'When I create an incall with DID "([^"]*)" in context "([^"]*)"')
+def when_i_create_incall_with_did(step, incall_did, context):
     open_url('incall', 'add')
-    incall_man.type_incall_did(did)
+    incall_man.type_incall_did(incall_did)
     incall_man.type_incall_context(context)
-    incall_man.type_incall_destination(destination_type, destination_name)
-    incall_man.type_incall_caller_id('"%s" <%s>' % (caller_id_name, caller_id_number))
     form.submit_form()
 
 
--- xivo_lettuce/manager_ws/incall_manager_ws.py
+++ xivo_lettuce/manager_ws/incall_manager_ws.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from lettuce import world
-from xivo_ws import Incall, UserDestination
+from xivo_ws import Incall, UserDestination, OverwriteCallerIDMode
 from xivo_lettuce.manager_ws import user_manager_ws
 
 
-def add_incall(number, context, dst_type, dst_name):
+def add_incall(number, context, dst_type, dst_name, caller_id=None):
     incall = Incall()
     incall.number = number
     incall.context = context
     incall.destination = _new_destination(dst_type, dst_name)
+    if caller_id:
+        incall.caller_id_mode = OverwriteCallerIDMode(caller_id)
     world.ws.incalls.add(incall)
 
 
