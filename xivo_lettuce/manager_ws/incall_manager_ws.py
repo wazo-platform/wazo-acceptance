f5db0890f70d13435605834c81281487d0b15ccc
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-11-02 11:11:57 -0400

    Add queue destination to incalls

--- xivo_lettuce/manager_ws/incall_manager_ws.py
+++ xivo_lettuce/manager_ws/incall_manager_ws.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from lettuce import world
-from xivo_ws import Incall, UserDestination, OverwriteCallerIDMode
+from xivo_ws import Incall, UserDestination, QueueDestination, OverwriteCallerIDMode
 from xivo_lettuce.manager_ws import user_manager_ws
+from xivo_lettuce.manager_ws import queue_manager_ws
 
 
 def add_incall(number, context, dst_type, dst_name, caller_id=None):
@@ -16,8 +17,11 @@ def add_incall(number, context, dst_type, dst_name, caller_id=None):
 
 
 def _new_destination(dst_type, dst_name):
+    dst_type = dst_type.lower()
     if dst_type == 'user':
         return _new_user_destination(dst_name)
+    elif dst_type == 'queue':
+        return _new_queue_destination(dst_name)
     else:
         raise Exception('unknown destination type %r' % dst_type)
 
@@ -26,6 +30,11 @@ def _new_user_destination(fullname):
     firstname, lastname = fullname.split()
     user_id = user_manager_ws.find_user_id_with_firstname_lastname(firstname, lastname)
     return UserDestination(user_id)
+
+
+def _new_queue_destination(queue_name):
+    queue_id = queue_manager_ws.find_queue_id_with_name(queue_name)
+    return QueueDestination(queue_id)
 
 
 def delete_incalls_with_did(incall_did):
