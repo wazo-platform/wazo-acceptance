145a545a28f0c9654c57ccba2d664fef4df520a2
Sebastien Duthil <sduthil@avencall.com>
2012-12-11 14:49:18 -0500

    import common instead of its functions

--- xivo_steps/call_steps.py
+++ xivo_steps/call_steps.py
@@ -3,7 +3,7 @@
 import time
 
 from lettuce import step, world
-from xivo_lettuce.common import open_url, element_is_in_list
+from xivo_lettuce import common
 from xivo_lettuce import form, func
 from xivo_lettuce.form.checkbox import Checkbox
 from xivo_lettuce.logs import search_str_in_asterisk_log
@@ -12,7 +12,7 @@ from xivo_lettuce.table import extract_webi_table_to_dict
 
 @step(u'Given there is "([^"]*)" activated in extenfeatures page')
 def given_there_is_group1_activated_in_extensions_page(step, option_label):
-    open_url('extenfeatures')
+    common.open_url('extenfeatures')
     option = Checkbox.from_label(option_label)
     option.check()
     form.submit.submit_form()
@@ -25,7 +25,8 @@ def then_i_not_see_recording_file_of_this_call_in_monitoring_audio_files_page(st
     nbtries = 0
     maxtries = 5
     while nbtries < maxtries:
-        assert not element_is_in_list('sounds', search % (now - nbtries), {'dir': 'monitor'})
+        file_name = search % (now - nbtries)
+        assert not common.element_is_in_list('sounds', file_name, {'dir': 'monitor'})
         nbtries += 1
 
 
@@ -38,7 +39,7 @@ def then_i_see_rejected_call_in_asterisk_log(step, extension):
 
 @step(u'Then I see the called extension "([^"]*)" by "([^"]*)" in call logs page')
 def then_i_see_the_called_extension_in_call_logs_page(step, called, caller):
-    open_url('cel')
+    common.open_url('cel')
     form.submit.submit_form()
 
     table = world.browser.find_element_by_xpath("//div[@id='sb-part-result']/div/table")
