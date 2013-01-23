ed82e342c7b8cc477ed9296567fcbe671074486c
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-01-23 16:00:02 -0500

    refactor queue manager, clean code and style

--- xivo_lettuce/manager/queue_manager.py
+++ xivo_lettuce/manager/queue_manager.py
@@ -15,56 +15,59 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from xivo_lettuce.manager_ws import queue_manager_ws, context_manager_ws
-from xivo_lettuce import form, sysutils
+from xivo_lettuce import sysutils
 from xivo_lettuce.common import open_url, go_to_tab
+from xivo_lettuce.form.input import set_text_field_with_label
+from xivo_lettuce.form.select import set_select_field_with_label
+from xivo_lettuce.form.list_pane import ListPane
+from xivo_lettuce.manager_ws import queue_manager_ws, context_manager_ws
 
 
-def remove_queues_with_name_or_number_if_exist(queue_name, queue_number):
+def remove_queues_with_name_or_number(queue_name, queue_number):
     queue_manager_ws.delete_queues_with_name(queue_name)
     queue_manager_ws.delete_queues_with_number(queue_number)
 
 
-def type_queue_name_display_name_number_context(name, display_name, extension, context):
-    form.input.set_text_field_with_label('Name', name)
-    form.input.set_text_field_with_label('Display name', display_name)
-    form.input.set_text_field_with_label('Number', extension)
-    context = context_manager_ws.get_context_with_name(context)
-    context_field_value = '%s (%s)' % (context.display_name, context.name)
-    form.select.set_select_field_with_label('Context', context_field_value)
-
-
 def type_queue_ring_strategy(ring_strategy):
-    form.select.set_select_field_with_label('Ring strategy', ring_strategy)
+    set_select_field_with_label('Ring strategy', ring_strategy)
 
 
 def add_or_replace_queue(queue):
-    remove_queues_with_name_or_number_if_exist(queue['name'], queue['number'])
+    remove_queues_with_name_or_number(queue['name'], queue['number'])
     open_url('queue', 'add')
     fill_form(queue)
 
 
 def fill_form(queue):
-    type_queue_name_display_name_number_context(queue['name'], queue['display name'],
-                                                queue['number'], queue['context'])
+    set_text_field_with_label('Name', queue['name'])
+    set_text_field_with_label('Display name', queue['display name'])
+    set_text_field_with_label('Number', queue['number'])
+
+    context = context_manager_ws.get_context_with_name(queue['context'])
+    context_field_value = '%s (%s)' % (context.display_name, context.name)
+    set_select_field_with_label('Context', context_field_value)
+
     if 'ring strategy' in queue:
         type_queue_ring_strategy(queue['ring strategy'])
+
     if 'agents' in queue:
         agentlist = queue['agents'].split(",")
         add_agents_to_queue(agentlist)
 
+
 def add_agents_to_queue(agents):
     go_to_tab('Members')
-    pane = form.list_pane.ListPane.from_id('agentlist')
+    pane = ListPane.from_id('agentlist')
     for agent in agents:
         pane.add_contains(agent)
 
 
 def remove_agents_from_queue(agents):
     go_to_tab('Members')
-    pane = form.list_pane.ListPane.from_id('agentlist')
+    pane = ListPane.from_id('agentlist')
     for agent in agents:
         pane.remove_contains(agent)
+
 
 def agent_numbers_from_asterisk(queue_name):
     output = _asterisk_queue_show(queue_name)
