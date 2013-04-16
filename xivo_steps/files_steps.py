9b4008bf947f0689045b7e276b1d5a4d31eba25b
Cedric Abunar <cabunar@avencall.com>
2013-04-16 09:40:28 -0400

    fix quotes in grep command

--- xivo_steps/files_steps.py
+++ xivo_steps/files_steps.py
@@ -90,7 +90,7 @@ def _get_asterisk_pid():
 @step(u'Then max open file descriptors are equals to 8192')
 def then_max_open_file_descriptors_are_equals_to_8192(step):
         pid = _get_asterisk_pid().strip()
-        cmd = ['/bin/grep', '"Max open files"', '/proc/%s/limits' % pid]
+        cmd = ['/bin/grep', 'Max open files', '/proc/%s/limits' % pid]
         string_limit = sysutils.output_command(cmd)
         limit = re.sub('\s+', ' ', string_limit).split()[3]
         assert int(limit) == 8192
