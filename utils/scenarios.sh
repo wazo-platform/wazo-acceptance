dd04e982f0572636ad47aa60208460e4308b3014
Cedric Abunar <cabunar@avencall.com>
2013-08-30 14:10:22 -0400

    add prompt for launch directly scenario number

--- utils/scenarios.sh
+++ utils/scenarios.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
 grep -i 'scenario:' $1 | sed 's/^ *Scenario: *//gI' | awk '{print NR " - " $0}'
+
+
+read -p 'That scenario you want to run ? number: ' number
+
+
+lettuce $1 -s${number}
