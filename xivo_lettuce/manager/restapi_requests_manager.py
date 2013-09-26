e7a55f8ae1c47c8a00757eacdfe823024d785646
Sebastien Duthil <sduthil@avencall.com>
2013-09-26 09:26:04 -0400

    explicit test failure

--- xivo_lettuce/manager/restapi_requests_manager.py
+++ xivo_lettuce/manager/restapi_requests_manager.py
@@ -18,8 +18,6 @@
 import re
 from urlparse import urlparse
 
-from hamcrest import assert_that, has_item
-from hamcrest.core.base_matcher import BaseMatcher
 from xivo_lettuce import logs
 
 DATE_PATTERN = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d+)\s*'
@@ -31,32 +29,8 @@ URL_PATTERN = r'([\w\:\./_\?\&\%=\-]+)\s*'
 DATA_PATTERN = r'(?:with data (.*))?'
 
 
-def has_request_infos(expected_request_infos):
-    return HasEntriesRegex(expected_request_infos)
-
-
-class HasEntriesRegex(BaseMatcher):
-    def __init__(self, expected_request_infos):
-        self.expected_request_infos = expected_request_infos
-
-    def _matches(self, item):
-        test_passed = True
-        for key, expected_value in self.expected_request_infos.iteritems():
-            test_passed = test_passed and (re.match(expected_value, item[key]) is not None)
-
-        return test_passed
-
-    def describe_to(self, description):
-        description.append_text('dict having entries matching').append_value(self.expected_request_infos)
-
-
-def assert_last_request_matches(**expected_request_infos):
-    last_requests = [_extract_request_infos(log_line) for log_line in logs.find_line_in_xivo_restapi_log(delta=20)]
-
-    if len(last_requests) == 0:
-        assert False, 'No logs found in %s' % logs.XIVO_RESTAPI_LOGFILE
-
-    assert_that(last_requests, has_item(has_request_infos(expected_request_infos)))
+def last_requests_infos():
+    return [_extract_request_infos(log_line) for log_line in logs.find_line_in_xivo_restapi_log(delta=20)]
 
 
 def _extract_request_infos(request):
--- xivo_steps/restapi_steps.py
+++ xivo_steps/restapi_steps.py
@@ -15,12 +15,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-
+import re
+from hamcrest import assert_that, has_item
+from hamcrest.core.base_matcher import BaseMatcher
 from lettuce import step
 from xivo_lettuce.manager import restapi_requests_manager
 
 
 @step(u'Then the REST API received a request with infos:$')
 def then_the_rest_api_received_a_request_with_infos(step):
-    for request_infos in step.hashes:
-        restapi_requests_manager.assert_last_request_matches(**request_infos)
+    last_requests = restapi_requests_manager.last_requests_infos()
+    for expected_request_infos in step.hashes:
+        assert_that(last_requests, has_item(has_request_infos(expected_request_infos)))
+
+
+def has_request_infos(expected_request_infos):
+    return HasEntriesRegex(expected_request_infos)
+
+
+class HasEntriesRegex(BaseMatcher):
+    def __init__(self, expected_request_infos):
+        self.expected_request_infos = expected_request_infos
+
+    def _matches(self, item):
+        test_passed = True
+        for key, expected_value in self.expected_request_infos.iteritems():
+            test_passed = test_passed and (re.match(expected_value, item[key]) is not None)
+
+        return test_passed
+
+    def describe_to(self, description):
+        description.append_text('dict having entries matching').append_value(self.expected_request_infos)
