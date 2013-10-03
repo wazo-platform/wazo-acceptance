d6d6bd0afb733ef7188b7a0f9b8e3a3517b024fb
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-10-03 14:02:03 -0400

    implement steps for REST API voicemail scenarios

--- xivo_lettuce/manager_dao/voicemail_manager_dao.py
+++ xivo_lettuce/manager_dao/voicemail_manager_dao.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from xivo_lettuce.remote_py_cmd import remote_exec
+from xivo_lettuce.remote_py_cmd import remote_exec, remote_exec_with_result
 
 
 def delete_voicemail_with_number_context(number, context):
@@ -49,3 +49,58 @@ def _delete_voicemail_with_user_id(channel, user_id):
     voicemail = voicemail_services.get(user.voicemailid)
 
     voicemail_services.delete(voicemail)
+
+
+def create_voicemail(parameters):
+    remote_exec(_create_voicemail, parameters=parameters)
+
+
+def _create_voicemail(channel, parameters):
+    from xivo_dao.alchemy.voicemail import Voicemail as VoicemailSchema
+    from xivo_dao.helpers.db_manager import AsteriskSession
+
+    voicemail = VoicemailSchema()
+
+    voicemail.fullname = parameters['name']
+    voicemail.mailbox = parameters['number']
+    voicemail.context = parameters['context']
+
+    if 'password' in parameters:
+        voicemail.password = parameters['password']
+
+    if 'email' in parameters:
+        voicemail.email = parameters['email']
+
+    if 'language' in parameters:
+        voicemail.language = parameters['language']
+
+    if 'timezone' in parameters:
+        voicemail.tz = parameters['timezone']
+
+    if 'max_messages' in parameters:
+        voicemail.maxmsg = int(parameters['max_messages'])
+
+    if 'attach_audio' in parameters:
+        voicemail.attach = int(parameters['attach_audio'])
+
+    if 'delete_messages' in parameters:
+        voicemail.deletevoicemail = int(parameters['delete_messages'])
+
+    if 'ask_password' in parameters:
+        voicemail.skipcheckpass = int(parameters['ask_password'])
+
+    AsteriskSession.begin()
+    AsteriskSession.add(voicemail)
+    AsteriskSession.commit()
+
+
+def total_voicemails():
+    return remote_exec_with_result(_total_voicemails)
+
+
+def _total_voicemails(channel):
+    from xivo_dao.alchemy.voicemail import Voicemail as VoicemailSchema
+    from xivo_dao.helpers.db_manager import AsteriskSession
+
+    count = AsteriskSession.query(VoicemailSchema).count()
+    channel.send(count)
--- xivo_steps/voicemail_steps.py
+++ xivo_steps/voicemail_steps.py
@@ -15,11 +15,131 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from lettuce import step
+from hamcrest import *
+from lettuce import step, world
 
-from xivo_lettuce.manager_ws import voicemail_manager_ws
+from xivo_lettuce.manager_ws import voicemail_manager_ws as old_voicemail_ws
+from xivo_lettuce.restapi.v1_1 import voicemail_helper
+from xivo_lettuce.manager_restapi import voicemail_ws
 
 
 @step(u'Given there is no voicemail "([^"]*)"')
 def given_there_is_no_voicemail_1(step, voicemail_number):
-    voicemail_manager_ws.delete_voicemails_with_number(voicemail_number)
+    old_voicemail_ws.delete_voicemails_with_number(voicemail_number)
+
+
+@step(u'Given I have no voicemail with id "([^"]*)"')
+def given_i_have_no_voicemail_with_id_group1(step, voicemail_id):
+    voicemail_helper.delete_voicemail_with_id(voicemail_id)
+
+
+@step(u'Given I have the following voicemails:')
+def given_have_the_following_voicemails(step):
+    for row in step.hashes:
+        voicemail_info = _extract_voicemail_info(row)
+        voicemail_helper.add_or_replace_voicemail(voicemail_info)
+
+
+def _extract_voicemail_info(row):
+    voicemail = dict(row)
+
+    if 'max_messages' in voicemail:
+        voicemail['max_messages'] = int(voicemail['max_messages'])
+
+    for key in ['attach_audio', 'delete_messages', 'ask_password']:
+        if key in voicemail:
+            voicemail[key] = (voicemail[key] == 'true')
+
+    return voicemail
+
+
+@step(u'When I request voicemail with id "([^"]*)"')
+def when_i_request_voicemail_with_id_group1(step, voicemail_id):
+    world.response = voicemail_ws.get_voicemail(voicemail_id)
+
+
+@step(u'When I send a request for the voicemail with number "([^"]*)", using its id')
+def when_i_send_a_request_for_the_voicemail_with_number_group1_using_its_id(step, number):
+    voicemail_id = voicemail_helper.find_voicemail_id_with_number(number)
+    world.response = voicemail_ws.get_voicemail(voicemail_id)
+
+
+@step(u'Then the voicemail has the following parameters:')
+def then_the_voicemail_has_the_following_parameters(step):
+    expected_voicemail = _extract_voicemail_info(step.hashes[0])
+    voicemail = world.response.data
+
+    assert_that(voicemail, has_entries(expected_voicemail))
+
+
+@step(u'When I request the list of voicemails$')
+def when_i_request_the_list_of_voicemails(step):
+    world.response = voicemail_ws.voicemail_list()
+
+
+@step(u'When I request the list of voicemails with the following parameters:')
+def when_i_request_the_list_of_voicemails_with_the_following_parameters(step):
+    parameters = step.hashes[0]
+    world.response = voicemail_ws.voicemail_list(parameters)
+
+
+@step(u'Then I get a list containing the following voicemails:')
+def then_i_get_a_list_containing_the_following_voicemails(step):
+    assert_that(world.response.data, has_entries(
+        'total', instance_of(int),
+        'items', instance_of(list)))
+
+    voicemail_list = world.response.data['items']
+
+    for voicemail in step.hashes:
+        voicemail = _extract_voicemail_info(voicemail)
+        assert_that(voicemail_list, has_item(has_entries(voicemail)))
+
+
+@step(u'Then I get a list of voicemails in the following order:')
+def then_i_get_a_list_of_voicemails_in_the_following_order(step):
+    expected_voicemails = [_extract_voicemail_info(v) for v in step.hashes]
+    matching_voicemails = _extract_matching_voicemails(expected_voicemails)
+    assert_that(matching_voicemails, _contains_all_voicemails(expected_voicemails))
+
+
+def _contains_all_voicemails(voicemails):
+    return contains(*_all_items(voicemails))
+
+
+def _extract_matching_voicemails(voicemails):
+    matcher = any_of(*_all_items(voicemails))
+    return [v for v in voicemails if matcher.matches(v)]
+
+
+def _all_items(voicemails):
+    return [has_entries(v) for v in voicemails]
+
+
+@step(u'Then I have a list with (\d+) of (\d+) results')
+def then_i_have_a_list_with_n_of_n_results(step, nb_list, nb_total):
+    nb_list = int(nb_list)
+    nb_total = int(nb_total)
+    assert_that(world.response.data, all_of(
+        has_entry('total', equal_to(nb_total)),
+        has_entry('items', has_length(nb_list))))
+
+
+@step(u'Then I have a list with (\d+) results$')
+def then_i_have_a_list_with_n_results(step, nb_list, nb_total):
+    nb_list = int(nb_list)
+    assert_that(world.response.data, has_entry('items', has_length(nb_list)))
+
+
+@step(u'Then the list contains the same total voicemails as on the server')
+def then_the_list_contains_the_same_total_voicemails_as_on_the_server(step):
+    total_server = voicemail_manager_dao.total_voicemails()
+    total_response = world.response.data['total']
+    assert_that(total_server, equal_to(total_response))
+
+
+@step(u'Then I do not have the following voicemails in the list:')
+def then_i_dot_not_have_the_following_voicemails_in_the_list(step):
+    expected_voicemails = [_extract_voicemail_info(v) for v in step.hashes]
+    matching_voicemails = _extract_matching_voicemails(expected_voicemails)
+    assert_that(matching_voicemails, is_not(_contains_all_voicemails(expected_voicemails)))
