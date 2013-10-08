899db2b99dc5a9630820daf6122336102668adb8
Cedric Abunar <cabunar@avencall.com>
2013-10-08 14:00:27 -0400

    move user_import_manager_ws to user_import_helper

--- xivo_steps/user_import_steps.py
+++ xivo_steps/user_import_steps.py
@@ -16,29 +16,29 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from lettuce import step
-from xivo_lettuce.manager_ws import user_import_manager_ws
+
+from xivo_acceptance.helpers import line_helper, user_helper, user_import_helper
 from xivo_lettuce import func
-from xivo_acceptance.helpers import line_helper, user_helper
 
 
 @step(u'^When I import a list of users with voicemail:$')
 def when_i_import_a_user_with_sip_line_and_voicemail(step):
-    user_import_manager_ws.insert_adv_user_with_mevo(step.hashes)
+    user_import_helper.insert_adv_user_with_mevo(step.hashes)
 
 
 @step(u'^When I import a list of users with incall:$')
 def when_i_import_a_user_with_sip_line_and_incall(step):
-    user_import_manager_ws.insert_adv_user_with_incall(step.hashes)
+    user_import_helper.insert_adv_user_with_incall(step.hashes)
 
 
 @step(u'^When I import a list of users with incall and voicemail - full:$')
 def when_i_import_a_user_with_sip_line_and_incall_and_voicemail_full(step):
-    user_import_manager_ws.insert_adv_user_full_infos(step.hashes)
+    user_import_helper.insert_adv_user_full_infos(step.hashes)
 
 
 @step(u'^When I import a list of users:$')
 def when_i_import_a_list_of_users(step):
-    user_import_manager_ws.insert_simple_user(step.hashes)
+    user_import_helper.insert_simple_user(step.hashes)
 
 
 @step(u'Then user with name "([^"]*)" exists')
