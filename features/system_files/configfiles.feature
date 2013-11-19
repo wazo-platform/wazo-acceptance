188052a5ebba685a7e5b7542d9bf4394e0626323
Sebastien Duthil <sduthil@avencall.com>
2013-11-19 16:40:29 -0500

    test checkbox for dialplan reload when editing config files

--- features/system_files/configfiles.feature
+++ features/system_files/configfiles.feature
@@ -4,3 +4,17 @@ Feature: Configuration Files
         Given no config file "testlink.conf"
         When I create a configfiles "testlink.conf" with content "[testlink]\n"
         Then configfiles "testlink.conf" is displayed in the list
+
+    Scenario: Add and edit a configuration file without reloading dialplan
+        Given no config file "test-no-dialplan-reload.conf"
+        When I create a config file "test-no-dialplan-reload.conf" without reloading dialplan
+        Then the dialplan has not been reloaded
+        When I edit the config file "test-no-dialplan-reload.conf" without reloading dialplan
+        Then the dialplan has not been reloaded
+
+    Scenario: Edit a configuration file and reload dialplan
+        Given no config file "test-dialplan-reload.conf"
+        When I create a config file "test-dialplan-reload.conf" and reload dialplan
+        Then the dialplan has been reloaded
+        When I edit the config file "test-dialplan-reload.conf" and reload dialplan
+        Then the dialplan has been reloaded
--- xivo_steps/configfiles_steps.py
+++ xivo_steps/configfiles_steps.py
@@ -15,8 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+from hamcrest import assert_that, contains_string, is_not, has_item
 from lettuce import step, world
-from xivo_lettuce import common, form
+from xivo_lettuce import common, form, logs
+from xivo_lettuce.form.checkbox import Checkbox
 
 
 @step(u'Given no config file "([^"]*)"')
@@ -34,3 +36,57 @@ def when_i_create_configfiles_with_content(step, filename, content):
     input_description.clear()
     input_description.send_keys(content)
     form.submit.submit_form()
+
+
+@step(u'When I create a config file "([^"]*)" without reloading dialplan')
+def when_i_create_a_config_file_group1_without_reloading_dialplan(step, file_name):
+    common.open_url('configfiles', 'add')
+    input_filename = world.browser.find_element_by_id('it-configfile-filename')
+    input_filename.clear()
+    input_filename.send_keys(file_name)
+    input_reload_dialplan = Checkbox(world.browser.find_element_by_id('it-configfile-reload-dialplan'))
+    input_reload_dialplan.uncheck()
+    form.submit.submit_form()
+
+
+@step(u'When I create a config file "([^"]*)" and reload dialplan')
+def when_i_create_a_config_file_group1_and_reload_dialplan(step, file_name):
+    common.open_url('configfiles', 'add')
+    input_filename = world.browser.find_element_by_id('it-configfile-filename')
+    input_filename.clear()
+    input_filename.send_keys(file_name)
+    input_reload_dialplan = Checkbox(world.browser.find_element_by_id('it-configfile-reload-dialplan'))
+    input_reload_dialplan.check()
+    form.submit.submit_form()
+
+
+@step(u'When I edit the config file "([^"]*)" without reloading dialplan')
+def when_i_edit_the_config_file_group1_without_reloading_dialplan(step, file_name):
+    common.open_url('configfiles')
+    common.edit_line(file_name)
+    input_reload_dialplan = Checkbox(world.browser.find_element_by_id('it-configfile-reload-dialplan'))
+    input_reload_dialplan.uncheck()
+    form.submit.submit_form()
+
+
+@step(u'When I edit the config file "([^"]*)" and reload dialplan')
+def when_i_edit_the_config_file_group1_and_reload_dialplan(step, file_name):
+    common.open_url('configfiles')
+    common.edit_line(file_name)
+    input_reload_dialplan = Checkbox(world.browser.find_element_by_id('it-configfile-reload-dialplan'))
+    input_reload_dialplan.check()
+    form.submit.submit_form()
+
+
+@step(u'Then the dialplan has not been reloaded')
+def then_the_dialplan_has_not_been_reloaded(step):
+    expected_command = "Asterisk command 'dialplan reload' successfully executed"
+    log_lines = logs.find_line_in_daemon_log()
+    assert_that(log_lines, is_not(has_item(contains_string(expected_command))))
+
+
+@step(u'Then the dialplan has been reloaded')
+def then_the_dialplan_has_been_reloaded(step):
+    expected_command = "Asterisk command 'dialplan reload' successfully executed"
+    log_lines = logs.find_line_in_daemon_log()
+    assert_that(log_lines, has_item(contains_string(expected_command)))
