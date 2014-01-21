18fd06d90b094ef2941251f7877610960dc12e1e
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-01-21 15:07:13 -0500

    implement steps for line scenarios

--- xivo_steps/restapi_1_1/lines_steps.py
+++ xivo_steps/restapi_1_1/lines_steps.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, has_key, has_entries, has_item
 from lettuce import step, world
 
 from xivo_acceptance.action.restapi import line_action_restapi
@@ -35,6 +35,12 @@ def given_i_have_the_following_lines(step):
     for lineinfo in step.hashes:
         _delete_line(lineinfo)
         _create_line(lineinfo)
+
+
+@step(u'Given I have no line with id "([^"]*)"')
+def given_i_have_no_line_with_id_group1(step, line_id):
+    line_id = int(line_id)
+    line_helper.delete_line(line_id)
 
 
 def _delete_line(lineinfo):
@@ -64,6 +70,12 @@ def when_i_ask_for_the_list_of_lines(step):
     world.response = line_action_restapi.all_lines()
 
 
+@step(u'When I ask for line with id "([^"]*)"')
+def when_i_ask_for_line_with_id_group1(step, line_id):
+    line_id = int(line_id)
+    world.response = line_action_restapi.get(line_id)
+
+
 @step(u'When I ask for the list of user_links with line_id "([^"]*)"$')
 def when_i_ask_for_the_list_of_user_links_with_line_id(step, line_id):
     world.response = line_action_restapi.all_user_links_by_line_id(line_id)
@@ -75,7 +87,33 @@ def then_the_line_group1_no_longer_exists(step, line_id):
     assert_that(response.status, equal_to(404))
 
 
-@step(u'Given I have no line with id "([^"]*)"')
-def given_i_have_no_line_with_id_group1(step, line_id):
-    line_id = int(line_id)
-    line_helper.delete_line(line_id)
+@step(u'Then I get a list containing the following lines:')
+def then_i_get_a_list_containing_the_following_lines(step):
+    line_response = world.response.data
+    expected_lines = step.hashes
+
+    assert_that(line_response, has_key('items'))
+    lines = line_response['items']
+
+    for expected_line in expected_lines:
+        expected_line = _convert_line_parameters(expected_line)
+        assert_that(lines, has_item(has_entries(expected_line)))
+
+
+def _convert_line_parameters(line):
+
+    if 'id' in line:
+        line['id'] = int(line['id'])
+
+    if 'device_slot' in line:
+        line['device_slot'] = int(line['device_slot'])
+
+    return line
+
+
+@step(u'Then I get a line with the following parameters:')
+def then_i_get_a_line_with_the_following_parameters(step):
+    expected_line = _convert_line_parameters(step.hashes[0])
+    line_response = world.response.data
+
+    assert_that(line_response, has_entries(expected_line))
