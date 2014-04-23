9080170b3d0c4162cc5917d6fa50a0e5e473db5a
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-04-23 16:00:51 -0400

    fix typo

--- xivo_acceptance/action/webi/meetme.py
+++ xivo_acceptance/action/webi/meetme.py
@@ -38,7 +38,7 @@ def fill_form(meetme):
     form.input.set_text_field_with_id('it-meetmefeatures-confno', meetme['number'])
 
     if 'context' in meetme:
-        form.select.set_select_field_with_id('it-meetmefeatures-maxusers', meetme['context'])
+        form.select.set_select_field_with_id_containing('it-meetmefeatures-context', meetme['context'])
 
     if 'max users' in meetme:
         form.input.set_text_field_with_id('it-meetmefeatures-maxusers', meetme['max users'])
