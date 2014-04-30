694840e2406a77b037e0b569667c3e31df6a282e
Cedric Abunar <cabunar@avencall.com>
2014-04-30 08:17:51 -0400

    fix userfeatures.description is not null

--- xivo_acceptance/helpers/user_helper.py
+++ xivo_acceptance/helpers/user_helper.py
@@ -95,6 +95,8 @@ def _create_user(channel, userinfo):
     from xivo_dao.data_handler.user.model import User
 
     user = User(**userinfo)
+    if not user.description:
+        user.description = ''
     user_services.create(user)
 
 
