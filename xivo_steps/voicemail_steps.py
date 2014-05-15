b8dc8c373cd69bd69c9a6acbf1e6a9f4d37a9fe9
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-15 10:35:17 -0400

    remove unused steps in voicemail scenarios

--- xivo_steps/voicemail_steps.py
+++ xivo_steps/voicemail_steps.py
@@ -21,7 +21,6 @@ from lettuce import step, world
 from xivo_acceptance.helpers import voicemail_helper, user_helper
 from xivo_acceptance.action.restapi import voicemail_action_restapi
 from xivo_acceptance.action.restapi import voicemail_link_action_restapi
-from xivo_lettuce.xivo_hamcrest import assert_has_dicts_in_order, assert_does_not_have_any_dicts
 from xivo_lettuce import func
 
 
@@ -86,12 +85,6 @@ def when_i_request_the_list_of_voicemails(step):
     world.response = voicemail_action_restapi.voicemail_list()
 
 
-@step(u'When I request the list of voicemails with the following parameters via RESTAPI:')
-def when_i_request_the_list_of_voicemails_with_the_following_parameters(step):
-    parameters = _extract_voicemail_info_to_restapi(step.hashes[0])
-    world.response = voicemail_action_restapi.voicemail_list(parameters)
-
-
 @step(u'When I link user "([^"]*)" with voicemail "([^"]*)" via RESTAPI')
 def when_i_link_user_group1_with_voicemail_group2_via_restapi(step, fullname, voicemail):
     user = user_helper.find_user_by_name(fullname)
@@ -117,14 +110,17 @@ def when_i_request_the_voicemail_associated_to_user_group1_group2_via_restapi(st
 def when_i_request_the_voicemail_associated_to_user_with_id_group1_via_restapi(step, user_id):
     world.response = voicemail_link_action_restapi.get_voicemail_link(int(user_id))
 
+
 @step(u'When I dissociate user "([^"]*)" "([^"]*)" from his voicemail via RESTAPI')
 def when_i_dissociate_user_group1_from_his_voicemail_via_restapi(step, firstname, lastname):
     user = user_helper.find_by_firstname_lastname(firstname, lastname)
     world.response = voicemail_link_action_restapi.delete_voicemail_link(int(user.id))
 
+
 @step(u'When I dissociate user with id "([^"]*)" from his voicemail via RESTAPI')
 def when_i_dissociate_user_with_id_group1_from_his_voicemail_via_restapi(step, user_id):
     world.response = voicemail_link_action_restapi.delete_voicemail_link(int(user_id))
+
 
 @step(u'Then I have the following voicemails via RESTAPI:')
 def then_the_voicemail_has_the_following_parameters(step):
@@ -146,41 +142,10 @@ def then_i_get_a_list_containing_the_following_voicemails(step):
         assert_that(voicemail_list, has_item(has_entries(voicemail)))
 
 
-@step(u'Then I get a list of voicemails in the following order via RESTAPI:')
-def then_i_get_a_list_of_voicemails_in_the_following_order(step):
-    all_voicemails = world.response.data['items']
-    expected_voicemails = [_extract_voicemail_info_to_restapi(v) for v in step.hashes]
-    assert_has_dicts_in_order(all_voicemails, expected_voicemails)
-
-
-@step(u'Then I have a list with (\d+) of (\d+) results')
-def then_i_have_a_list_with_n_of_n_results(step, nb_list, nb_total):
-    nb_list = int(nb_list)
-    nb_total = int(nb_total)
-    assert_that(world.response.data, all_of(
-        has_entry('total', equal_to(nb_total)),
-        has_entry('items', has_length(nb_list))))
-
-
 @step(u'Then I have a list with (\d+) results$')
 def then_i_have_a_list_with_n_results(step, nb_list):
     nb_list = int(nb_list)
     assert_that(world.response.data, has_entry('items', has_length(nb_list)))
-
-
-@step(u'Then the list contains the same total voicemails as on the server')
-def then_the_list_contains_the_same_total_voicemails_as_on_the_server(step):
-    total_server = voicemail_helper.total_voicemails()
-    total_response = world.response.data['total']
-    assert_that(total_server, equal_to(total_response))
-
-
-@step(u'Then I do not have the following voicemails in the list:')
-def then_i_dot_not_have_the_following_voicemails_in_the_list(step):
-    all_voicemails = world.response.data['items']
-    not_expected_voicemails = [_extract_voicemail_info_from_restapi(v) for v in step.hashes]
-
-    assert_does_not_have_any_dicts(all_voicemails, not_expected_voicemails)
 
 
 @step(u'Then voicemail with number "([^"]*)" no longer exists')
