a1b602ad7c2ebbe0de356c5d149d59d83088b684
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-15 10:12:24 -0400

    add option to scenarios.sh to only print list of scenarios

--- utils/scenarios.sh
+++ utils/scenarios.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
 
-grep -i 'scenario:' $1 | sed 's/^ *Scenario: *//gI' | awk '{print NR " - " $0}'
+usage() {
+    echo "Usage: $0 [-p] LETTUCE_FILE"
+    echo
+    echo "Options:"
+    echo "  -p  only print the list of scenarios, do not run them"
+    echo "  -h  print this help"
+    exit 1
+}
 
+print_scenarios() {
+    file=$1
+    grep -i 'scenario\( outline\)\?:' $file | sed 's/^ *Scenario\( Outline\)\?: *//gI' | awk '{print NR " - " $file}'
+}
 
-read -p 'That scenario you want to run ? number: ' number
+execute_scenario() {
+    file=$1
+    read -p 'Which scenario do you want to run ? number: ' number
+    lettuce $file -s${number}
+}
 
+RUN_LETTUCE=1
 
-lettuce $1 -s${number}
+while getopts ":hp" opt; do
+    case "$opt" in
+        p)
+            RUN_LETTUCE=0
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            usage
+            ;;
+        esac
+done
+
+LETTUCE_FILE=${@:$OPTIND}
+
+print_scenarios $LETTUCE_FILE
+
+if [ $RUN_LETTUCE = 1 ]; then
+    execute_scenario $LETTUCE_FILE
+fi
