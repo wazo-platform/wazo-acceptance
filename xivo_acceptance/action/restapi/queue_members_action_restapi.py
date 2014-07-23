fa797cef0396ac01e527a780b95b8c2ea02f3f78
Jean Aunis <jaunis@avencall.com>
2014-07-23 16:36:40 +0200

    created the step to get the queue members of type agent with restapi

---
+++ xivo_acceptance/action/restapi/queue_members_action_restapi.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+from lettuce.registry import world
+
+AGENT_QUEUE_MEMBERS_PATH = 'queues/%s/memberships/agents/%s'
+
+
+def get_agent_queue_association(queue_id, agent_id):
+    return world.restapi_utils_1_1.rest_get(AGENT_QUEUE_MEMBERS_PATH % (queue_id, agent_id))
---
+++ xivo_steps/queue_members_steps.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+from lettuce.decorators import step
+from lettuce.registry import world
+from xivo_acceptance.action.restapi import queue_members_action_restapi
+from xivo_acceptance.helpers import queue_helper, agent_helper
+
+
+@step(u'When I request the queue member information for the queue "([^"]*)" and the agent "([^"]*)"')
+def when_i_request_the_queue_member_information_for_the_queue_group1_and_the_agent_group2(step, queue_name, agent_number):
+    queue_id = queue_helper.find_queue_id_with_name(queue_name)
+    agent_id = agent_helper.find_agent_id_with_number(agent_number)
+    world.response = queue_members_action_restapi.get_agent_queue_association(queue_id, agent_id)
