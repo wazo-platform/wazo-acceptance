da3d13efe3a79b6077d94c9a2866ab6914c75667
Sebastien Duthil <sduthil@avencall.com>
2015-01-14 10:50:46 -0500

    usesetuptools.find_packages

--- setup.py
+++ setup.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from distutils.core import setup
 import fnmatch
 import os
 
-
-def is_package(path):
-    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
-    is_test_module = fnmatch.fnmatch(path, '*tests')
-    return not (is_svn_dir or is_test_module)
-
-
-def packages_in(package):
-    return [p for p, _, _ in os.walk(package) if is_package(p)]
-
-
-def files_in(directory):
-    for dir, _, files in os.walk(directory):
-        for file in files:
-            yield '{dir}/{file}'.format(dir=dir, file=file)
+from setuptools import setup
+from setuptools import find_packages
 
 
 def data(source_dir, dest_dir):
@@ -27,10 +13,6 @@ def data(source_dir, dest_dir):
         relpath = os.path.relpath(root, source_dir)
         newpath = os.path.join(dest_dir, relpath)
         yield (newpath, [os.path.join(root, filename) for filename in filenames])
-
-
-packages = (packages_in('xivo_acceptance'))
-confd = list(files_in('etc/xivo-acceptance/conf.d'))
 
 setup(
     name='xivo-acceptance',
@@ -40,7 +22,7 @@ setup(
     author_email='dev@avencall.com',
     url='https://github.com/xivo-pbx/xivo-acceptance',
     license='GPLv3',
-    packages=packages,
+    packages=find_packages(),
     scripts=[
         'bin/xivo-acceptance',
     ],
