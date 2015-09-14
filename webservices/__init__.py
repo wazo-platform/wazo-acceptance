2624273788a5220c6b8dd9936c453b97a6d344de
Etienne Lessard <elessard@avencall.com>
2015-09-14 08:24:33 -0400

    webservices: also disable ssl cert verification

--- webservices/__init__.py
+++ webservices/__init__.py
@@ -14,3 +14,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
