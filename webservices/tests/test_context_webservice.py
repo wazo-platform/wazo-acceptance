5d2dd18640d29614c7a40d6a48adfd2e1bd0efc3
Etienne Lessard <elessard@avencall.com>
2015-12-05 10:13:43 -0500

    update edit context test
    Context name edition is not possible anymore, so update the test so that
    it only tries to update the display name of the context, not the name.

--- webservices/tests/test_context_webservice.py
+++ webservices/tests/test_context_webservice.py
@@ -36,15 +36,13 @@ class TestContextWebServices(unittest.TestCase):
         self.assertEqual(common.nb_with_name('contexts', context.name), 1)
 
     def test_edit_context(self):
-        common.delete_with_name('contexts', u'test_ws_edit_context')
-        self._add_context(u'test_ws_add_context')
-        context = common.find_with_name('contexts', u'test_ws_add_context')[0]
-        context.name = u'test_ws_edit_context'
-        context.display_name = u'test_ws_edit_context'
+        self._add_context(u'test_ws_edit_context')
+        context = common.find_with_name('contexts', u'test_ws_edit_context')[0]
+        context.display_name = u'foobar'
         self._xivo_ws.contexts.edit(context)
         context = common.find_with_name('contexts', u'test_ws_edit_context')[0]
 
-        self.assertEqual(context.name, u'test_ws_edit_context')
+        self.assertEqual(context.display_name, u'foobar')
 
     def test_delete_context(self):
         self._add_context(u'test_ws_delete_context')
