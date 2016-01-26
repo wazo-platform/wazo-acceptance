b8f57ab9a431bd792af2280bc8d51a06c8016ca0
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2016-01-26 08:26:42 -0500

    remove accesswebservice test with login admin in prereq

--- data/assets/webservices.sql
+++ data/assets/webservices.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
+DELETE FROM "accesswebservice" WHERE NAME = 'test' AND login = 'admin';
 INSERT INTO "accesswebservice" (name, login, passwd, description) VALUES ('test', 'admin', 'proformatique', '');
 
 COMMIT;
