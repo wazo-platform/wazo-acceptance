2f4ee68e59cf86135675e760a8f1f924643b4875
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2016-01-26 07:34:44 -0500

    prerequisite: stop removing all accesswebservice

--- data/assets/webservices.sql
+++ data/assets/webservices.sql
@@ -1,10 +1,7 @@
-
-
 \connect asterisk;
 
 BEGIN;
 
-DELETE FROM "accesswebservice";
 INSERT INTO "accesswebservice" (name, login, passwd, description) VALUES ('test', 'admin', 'proformatique', '');
 
 COMMIT;
