4461d474ae7fe42528f9079a129be173f012a9a2
Sebastien Duthil <sduthil@proformatique.com>
2016-02-18 16:06:40 -0500

    user_line_steps: remove unused steps

--- xivo_acceptance/steps/user_line_steps.py
+++ xivo_acceptance/steps/user_line_steps.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013-2014 Avencall
+# Copyright (C) 2013-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,29 +22,6 @@ from xivo_acceptance.action.confd import user_line_action_confd
 from xivo_acceptance.helpers import user_helper, line_sip_helper
 
 
-@step(u'Given I have no user_line with the following parameters:')
-def given_i_have_no_user_line_with_the_following_parameters(step):
-    for data in step.hashes:
-        user_line = _extract_parameters(data)
-        world.response = user_line_action_confd.delete_user_line(user_line['user_id'], user_line['line_id'])
-
-
-@step(u'Given line "([^"]*)" is linked with user "([^"]*)" "([^"]*)"')
-def given_line_group1_is_linked_with_user_group2_group3(step, line_id, firstname, lastname):
-    line_id = int(line_id)
-    user_id = user_helper.get_user_id_with_firstname_lastname(firstname, lastname)
-    world.response = user_line_action_confd.create_user_line(user_id, {'line_id': line_id})
-    assert_that(world.response.status, equal_to(201), unicode(world.response.data))
-
-
-@step(u'Given line "([^"]*)" is linked with user id "([^"]*)"')
-def given_line_group1_is_linked_with_user_id_group2(step, line_id, user_id):
-    line_id = int(line_id)
-    user_id = int(user_id)
-    world.response = user_line_action_confd.create_user_line(user_id, {'line_id': line_id})
-    assert_that(world.response.status, equal_to(201), unicode(world.response.data))
-
-
 @step(u'Given SIP line "([^"]*)" is associated to user "([^"]*)" "([^"]*)"')
 def given_sip_line_group1_is_associated_to_user_group2_group3(step, sip_username, firstname, lastname):
     line = line_sip_helper.find_by_username(sip_username)
@@ -52,43 +29,3 @@ def given_sip_line_group1_is_associated_to_user_group2_group3(step, sip_username
     user_id = user_helper.get_user_id_with_firstname_lastname(firstname, lastname)
     world.response = user_line_action_confd.create_user_line(user_id, {'line_id': line['id']})
     assert_that(world.response.status, equal_to(201), unicode(world.response.data))
-
-
-@step(u'When I create an empty user_line')
-def when_i_create_an_empty_user_line(step):
-    world.response = user_line_action_confd.create_user_line(1, {})
-
-
-@step(u'When I create the following user_line via CONFD:')
-def when_i_create_the_following_user_lines(step):
-    for data in step.hashes:
-        user_line = _extract_parameters(data)
-        world.response = user_line_action_confd.create_user_line(user_line['user_id'], user_line)
-
-
-@step(u'When I request the lines associated to user id "([^"]*)" via CONFD')
-def when_i_request_the_lines_associated_to_user_id_group1_via_confd(step, user_id):
-    world.response = user_line_action_confd.get_user_line(user_id)
-
-
-@step(u'When I dissociate the following user_line via CONFD:')
-def when_i_dossociate_the_following_user_lines(step):
-    for data in step.hashes:
-        user_line = _extract_parameters(data)
-        world.response = user_line_action_confd.delete_user_line(user_line['user_id'], user_line['line_id'])
-
-
-def _extract_parameters(user_line):
-    if 'user_id' in user_line and user_line['user_id'].isnumeric():
-        user_line['user_id'] = int(user_line['user_id'])
-
-    if 'line_id' in user_line and user_line['line_id'].isnumeric():
-        user_line['line_id'] = int(user_line['line_id'])
-
-    if 'main_line' in user_line and user_line['main_line'] in ['True', 'False']:
-        user_line['main_line'] = user_line['main_line'] == 'True'
-
-    if 'main_user' in user_line and user_line['main_user'] in ['True', 'False']:
-        user_line['main_user'] = user_line['main_user'] == 'True'
-
-    return user_line
