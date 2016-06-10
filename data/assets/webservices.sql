db4e1c54d7f130f08288d0387f4aa454d52a93ef
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-06-10 10:23:48 -0400

    add a ctid-ng client

--- data/assets/webservices.sql
+++ data/assets/webservices.sql
@@ -3,7 +3,7 @@
 BEGIN;
 
 DELETE FROM "accesswebservice" WHERE NAME = 'xivo-acceptance' AND login = 'xivo-acceptance';
-INSERT INTO "accesswebservice" (name, login, passwd, acl) VALUES ('xivo-acceptance', 'xivo-acceptance', 'proformatique', '{confd.#, dird.#, agentd.#}');
+INSERT INTO "accesswebservice" (name, login, passwd, acl) VALUES ('xivo-acceptance', 'xivo-acceptance', 'proformatique', '{confd.#, dird.#, agentd.#, ctid-ng.#}');
 DELETE FROM "accesswebservice" WHERE NAME = 'test' AND login = 'admin';
 INSERT INTO "accesswebservice" (name, login, passwd, description) VALUES ('test', 'admin', 'proformatique', '');
 
