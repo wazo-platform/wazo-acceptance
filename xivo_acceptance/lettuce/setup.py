969f6e481db345c2a72f6710f13ec72e587b7870
Etienne Lessard <elessard@proformatique.com>
2016-06-20 07:57:51 -0400

    fix syntax error
    This is a bit embarrassing.

--- xivo_acceptance/lettuce/setup.py
+++ xivo_acceptance/lettuce/setup.py
@@ -42,8 +42,8 @@ def setup_confd_client():
 
 
 def setup_ctid_ng_client():
-    world.ctid_ng_client = CtidNgClient(**world.config['ctid_ng'],
-                                        token=world.config.get('auth_token'))
+    world.ctid_ng_client = CtidNgClient(**world.config['ctid_ng'])
+    world.confd_client.set_token(world.config.get('auth_token'))
 
 
 def setup_auth_token():
