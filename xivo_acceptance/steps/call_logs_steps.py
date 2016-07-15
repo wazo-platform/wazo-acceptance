3245e23ac13dbb3f3abc618d9b703869a03b3520
Sebastien Duthil <sduthil@proformatique.com>
2016-07-15 16:07:11 -0400

    extract confd test to xivo-confd integration tests

--- xivo_acceptance/steps/call_logs_steps.py
+++ xivo_acceptance/steps/call_logs_steps.py
@@ -15,28 +15,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from StringIO import StringIO
+
 from datetime import timedelta
 from time import strptime
 
 from hamcrest import all_of
-from hamcrest import any_of
 from hamcrest import assert_that
 from hamcrest import ends_with
-from hamcrest import equal_to
-from hamcrest import greater_than
 from hamcrest import has_entries
 from hamcrest import has_item
-from hamcrest import has_length
 from hamcrest import has_property
 from lettuce import step, world
-from xivo.unicode_csv import UnicodeDictReader
-from xivo_acceptance.action.confd import call_logs_action_confd
-from xivo_acceptance.action.webi import call_logs as call_logs_action_webi
-from xivo_acceptance.action.webi import common as common_action_webi
 from xivo_acceptance.helpers import call_logs_helper, cel_helper
 from xivo_acceptance.helpers.datetime_helper import close_to
-from xivo_acceptance.lettuce import assets, common, form, sysutils
+from xivo_acceptance.lettuce import assets, common, sysutils
 
 
 @step(u'Given there are no call logs$')
@@ -65,18 +57,6 @@ def given_there_are_a_lot_of_calls(step):
 def given_there_are_only_the_following_call_logs(step):
     call_logs_helper.delete_all()
     call_logs_helper.create_call_logs(step.hashes)
-
-
-@step(u'When I get the list of call logs$')
-def when_i_get_the_list_of_call_logs(step):
-    world.response = call_logs_action_confd.call_logs_list()
-
-
-@step(u'When I get the list of call logs with arguments:')
-def when_i_get_the_list_of_call_logs_with_arguments(step):
-    args = step.hashes[0]
-    world.response = call_logs_action_confd.call_logs_list_interval(args)
-    world.status = world.response.status
 
 
 @step(u'When I generate call logs$')
@@ -148,19 +128,3 @@ def then_i_have_the_last_call_log_matching(step):
 def then_i_should_not_have_the_following_call_logs(step):
     for entry in step.hashes:
         assert not call_logs_helper.has_call_log(entry), "Corresponding call_log entry was found : %s" % entry
-
-
-@step(u'Then I get the following call logs in CSV format:')
-def then_i_get_the_following_call_logs_in_csv_format(step):
-    assert_that(world.response.status, equal_to(200))
-    assert_that(world.response.headers['Content-Type'], equal_to('text/csv; charset=utf-8'))
-
-    call_logs_response = world.response.data.encode('utf-8')
-    assert_that(call_logs_response, has_length(greater_than(0)))
-
-    reader = UnicodeDictReader(StringIO(call_logs_response))
-    row_matchers = [has_entries(expected_row) for expected_row in step.hashes]
-    csv_rows = [csv_row for csv_row in reader]
-    assert_that(csv_rows, has_length(len(step.hashes)))
-    for csv_row_dict in csv_rows:
-        assert_that(csv_row_dict, any_of(*row_matchers))
