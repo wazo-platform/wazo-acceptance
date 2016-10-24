7837aa8df6bf085be359a8b2862c04c1749dab79
Francois Blackburn <fblackburn@proformatique.com>
2016-10-24 15:25:52 -0400

    trunk_sip: separate webi and confd

--- data/features/daily/protocol_sip/trunk_sip.feature
+++ data/features/daily/protocol_sip/trunk_sip.feature
@@ -1,13 +1,11 @@
 Feature: TrunkSIP
 
-    Scenario: Add a trunksip
+    Scenario: Add a trunk sip
         Given there is no trunksip "tokyo_paris"
         When I create a trunksip with name "tokyo_paris"
-        Then trunksip "tokyo_paris" is displayed in the list
         Then the "sip.conf" file should contain peer "tokyo_paris"
 
-    Scenario: Remove a trunksip
+    Scenario: Remove a trunk sip
         Given there is a trunksip "tokyo_paris"
         When I remove the trunksip "tokyo_paris"
-        Then trunksip "tokyo_paris" is not displayed in the list
         Then the "sip.conf" file should not contain peer "tokyo_paris"
--- xivo_acceptance/steps/trunk_sip_steps.py
+++ xivo_acceptance/steps/trunk_sip_steps.py
@@ -34,13 +34,25 @@ def given_there_is_a_trunksip(step, name):
 
 @step(u'When I create a trunksip with name "([^"]*)"')
 def when_i_create_a_trunksip_with_name_and_trunk(step, name):
+    endpoint_sip = world.confd_client.endpoints_sip.create({'username': name})
+    trunk = world.confd_client.trunks.create({})
+    world.confd_client.trunks(trunk).add_endpoint_sip(endpoint_sip)
+
+
+@step(u'When I remove the trunksip "([^"]*)"')
+def when_i_remove_the_trunksip(step, name):
+    trunksip_helper.delete_trunksips_with_name(name)
+
+
+@step(u'When I create a trunksip with name "([^"]*)" in the webi')
+def when_i_create_a_trunksip_with_name_and_trunk_in_the_webi(step, name):
     common.open_url('trunksip', 'add')
     input_name = world.browser.find_element_by_id('it-protocol-name', 'trunksip form not loaded')
     input_name.send_keys(name)
     form.submit.submit_form()
 
 
-@step(u'When I remove the trunksip "([^"]*)"')
-def when_i_remove_the_trunksip(step, name):
+@step(u'When I remove the trunksip "([^"]*)" in the webi')
+def when_i_remove_the_trunksip_in_the_webi(step, name):
     common.open_url('trunksip', 'list')
     common.remove_line(name)
