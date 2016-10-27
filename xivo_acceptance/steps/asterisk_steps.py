d3032877878f1e0f050549ff1300ed7937bfe5d8
Francois Blackburn <fblackburn@proformatique.com>
2016-10-27 09:44:06 -0400

    remove unused import

--- xivo_acceptance/steps/asterisk_steps.py
+++ xivo_acceptance/steps/asterisk_steps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2013-2016 Avencall
+# Copyright (C) 2016 Proformatique Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import os
 import re
 import time
 
