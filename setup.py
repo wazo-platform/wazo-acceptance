930ee6ecc360392441cd3b96cf25bc9b058e316c
Sebastien Duthil <sduthil@wazo.community>
2017-05-01 14:56:01 -0400

    Revert "remove useless setup.py"
    This reverts commit 43d40fd1ae840b1901cfd227a2912a0025ca1d16.

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+import fnmatch
+import os
+
+
+def is_package(path):
+    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
+    is_test_module = fnmatch.fnmatch(path, '*tests')
+    return not (is_svn_dir or is_test_module)
+
+
+def packages_in(package):
+    return [p for p, _, _ in os.walk(package) if is_package(p)]
+
+
+def files_in(directory):
+    for dir, _, files in os.walk(directory):
+        for file in files:
+            yield '{dir}/{file}'.format(dir=dir, file=file)
+
+
+def data(source_dir, dest_dir):
+    for root, dirnames, filenames in os.walk(source_dir):
+        relpath = os.path.relpath(root, source_dir)
+        newpath = os.path.join(dest_dir, relpath)
+        yield (newpath, [os.path.join(root, filename) for filename in filenames])
+
+
+packages = (packages_in('xivo_acceptance'))
+confd = list(files_in('etc/xivo-acceptance/conf.d'))
+
+setup(
+    name='xivo-acceptance',
+    version='0.1',
+    description='XiVO Acceptance',
+    author='Wazo Authors',
+    author_email='dev.wazo@gmail.com',
+    url='http://wazo.community',
+    license='GPLv3',
+    packages=packages,
+    scripts=[
+        'bin/xivo-acceptance',
+    ],
+    data_files=list(
+        data('data', 'share/xivo-acceptance')
+    )
+)
