5bf6000ef7bc2d397d58faeaea6c31c3e871e988
Pascal Cadotte Michaud <pcm@wazo.io>
2018-06-22 11:20:05 -0400

    add debug on outcalls

--- xivo_acceptance/helpers/outcall_helper.py
+++ xivo_acceptance/helpers/outcall_helper.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
-# Copyright (C) 2016 Proformatique Inc.
+# Copyright 2013-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
+
 from lettuce import world
+
+logger = logging.getLogger(__name__)
 
 
 def add_or_replace_outcall(data):
@@ -13,6 +16,10 @@ def add_or_replace_outcall(data):
 
 def add_outcall(data):
     outcall = world.confd_client.outcalls.create({'name': data['name']})
+    logger.critical('adding extensions')
+    logger.critical('tenant id: %s', world.confd_client._tenant_id)
+    logger.critical('default_tenant: %s', world.confd_client._default_tenant_id)
+    logger.critical('token: %s', world.confd_client._token_id)
     for extension in data.get('extensions', []):
         extension_confd = world.confd_client.extensions.create({'exten': extension['exten'],
                                                                 'context': extension['context']})
