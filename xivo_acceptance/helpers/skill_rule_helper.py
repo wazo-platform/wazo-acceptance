4a64fc4b5c0fa7052f4203c1f06008cf236804ae
Pascal Cadotte Michaud <pcm@wazo.io>
2018-07-20 11:52:34 -0400

    skill rules: use confd instead of ws

--- xivo_acceptance/helpers/skill_rule_helper.py
+++ xivo_acceptance/helpers/skill_rule_helper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from lettuce.registry import world
@@ -7,8 +7,8 @@ from lettuce.registry import world
 
 def delete_skill_rules_with_name(name):
     for skill_rule in _search_skill_rules_with_name(name):
-        world.ws.queueskillrules.delete(skill_rule.id)
+        world.confd_client.queue_skill_rules.delete(skill_rule['id'])
 
 
 def _search_skill_rules_with_name(name):
-    return world.ws.queueskillrules.search_by_name(name)
+    return world.confd_client.queue_skill_rules.list(name=name)['items']
