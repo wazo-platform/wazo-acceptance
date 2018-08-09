8f6872ee02a2a1990317f3963bb0c5e971f54ea5
Francois Blackburn <fblackburn@wazo.community>
2018-08-09 08:05:03 -0400

    service_extensions steps: use xivo-confd API

--- xivo_acceptance/steps/service_extensions.py
+++ xivo_acceptance/steps/service_extensions.py
@@ -1,63 +1,36 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014-2016 Avencall
+# Copyright 2014-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from lettuce import step
+from lettuce.registry import world
 
-from xivo_acceptance.lettuce import common
-from xivo_acceptance.lettuce import form
-from xivo_acceptance.lettuce.form.checkbox import Checkbox
-from xivo_acceptance.lettuce.form.input import set_text_field_with_id
-
-_EXTENSIONS = {
-    'Enable forwarding on no-answer': {
-        'tab': ['General', 'Forwards'],
-        'checkbox_id': 'it-extenfeatures-enable-fwdrna',
-        'text_id': 'it-extenfeatures-fwdrna',
-    },
-    'Enable forwarding on busy': {
-        'tab': ['General', 'Forwards'],
-        'checkbox_id': 'it-extenfeatures-enable-fwdbusy',
-        'text_id': 'it-extenfeatures-fwdbusy',
-    },
-    'Enable unconditional forwarding': {
-        'tab': ['General', 'Forwards'],
-        'checkbox_id': 'it-extenfeatures-enable-fwdunc',
-        'text_id': 'it-extenfeatures-fwdunc',
-    },
-    'Do not disturb': {
-        'tab': ['General'],
-        'checkbox_id': 'it-extenfeatures-enable-enablednd',
-        'test_id': 'it-extenfeatures-enablednd',
-    },
-    'Incoming call filtering': {
-        'tab': ['General'],
-        'checkbox_id': 'it-extenfeatures-enable-incallfilter',
-        'test_id': 'it-extenfeatures-incallfilter',
-    },
+_DESCRIPTION_TO_FEATURE = {
+    'Enable forwarding on no-answer': 'fwdrna',
+    'Enable forwarding on busy': 'fwdbusy',
+    'Enable unconditional forwarding': 'fwdunc',
+    'Do not disturb': 'enablednd',
+    'Incoming call filtering': 'incallfilter',
 }
 
 
 @step(u'Given the "([^"]*)" extension is (disabled|enabled)')
-def given_the_extension_is_disabled(step, exten_name, state):
-    extension = _EXTENSIONS[exten_name]
-    _open_url_and_go_to_tab(extension)
-    if state == 'enabled':
-        Checkbox.from_id(extension['checkbox_id']).check()
-    else:
-        Checkbox.from_id(extension['checkbox_id']).uncheck()
-    form.submit.submit_form()
+def given_the_extension_is_disabled(step, description, state):
+    feature = _DESCRIPTION_TO_FEATURE[description]
+    extension = world.confd_client.extensions_features.list(feature=feature)['items'][0]
+    extension['enabled'] = True if state == 'enabled' else False
+    world.confd_client.extensions_features.update(extension)
 
 
 @step(u'Given the "([^"]*)" extension is set to "([^"]*)"')
-def given_the_extension_is_set_to(step, exten_name, exten_value):
-    extension = _EXTENSIONS[exten_name]
-    _open_url_and_go_to_tab(extension)
-    Checkbox.from_id(extension['checkbox_id']).check()
-    set_text_field_with_id(extension['text_id'], exten_value)
-    form.submit.submit_form()
+def given_the_extension_is_set_to(step, description, exten):
+    feature = _DESCRIPTION_TO_FEATURE[description]
+    extension = world.confd_client.extensions_features.list(feature=feature)['items'][0]
+    extension['exten'] = _convert_to_pattern_if_needed(exten)
+    world.confd_client.extensions_features.update(extension)
 
 
-def _open_url_and_go_to_tab(extension):
-    common.open_url('extensions')
-    common.go_to_tab(*extension['tab'])
+def _convert_to_pattern_if_needed(exten):
+    if exten.endswith('.'):
+        return '_{}'.format(exten)
+    return exten
