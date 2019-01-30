d52452057f7c7db96014299cb78ee80ae3a43af1
Alexandre Fournier <afournier@wazo.io>
2019-01-30 16:46:10 -0500

    provd_helper: add functions to replace webi actions

--- xivo_acceptance/helpers/provd_helper.py
+++ xivo_acceptance/helpers/provd_helper.py
@@ -4,8 +4,10 @@
 
 import urllib2
 from lettuce import world
-from hamcrest import assert_that, is_not, none, has_entry, has_entries, has_key
+from hamcrest import assert_that, is_not, none, has_entry, has_entries, has_key, is_
+from xivo_test_helpers import until
 from wazo_provd_client.exceptions import ProvdError
+from wazo_provd_client import operation
 import uuid
 
 
@@ -111,6 +113,44 @@ def _find_by(key, value):
     device_manager = world.provd_client.devices
     devices = device_manager.list({key: value})['devices']
     return devices[0] if devices else None
+
+
+def update_plugin_list(url):
+    world.provd_client.params.update('plugin_server', url)
+
+
+def get_latest_plugin_name(plugin_name):
+    all_plugins = world.provd_client.plugins.list_installable()['pkgs']
+    matching_plugins = [plugin for plugin in all_plugins if plugin_name in plugin]
+    if not matching_plugins:
+        raise Exception('no matching plugins for {}'.format(plugin_name))
+    return max(matching_plugins)
+
+
+def install_latest_plugin(plugin):
+    latest_plugin = get_latest_plugin_name(plugin)
+    plugins_installed = world.provd_client.plugins.list_installed()['pkgs']
+    if latest_plugin not in plugins_installed:
+        install_plugin(latest_plugin)
+
+
+def install_plugin(plugin_name):
+    with world.provd_client.plugins.install(plugin_name) as operation_progress:
+        until.assert_(
+            operation_successful, operation_progress, tries=20, interval=0.5
+        )
+
+
+def plugins_successfully_updated():
+    with world.provd_client.plugins.update() as operation_progress:
+        until.assert_(
+            operation_successful, operation_progress, tries=20, interval=0.5
+        )
+
+
+def operation_successful(operation_ressource):
+    operation_ressource.update()
+    assert_that(operation_ressource.state, is_(operation.OIP_SUCCESS))
 
 
 def request_http(path, user_agent):
