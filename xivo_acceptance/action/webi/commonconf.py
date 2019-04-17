e2e4bafd6c77a7be04b8159e2c8d34c3054ebddd
Francois Blackburn <fblackburn@wazo.community>
2019-04-17 19:00:26 -0400

    remove action webi

--- xivo_acceptance/action/webi/commonconf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from xivo_acceptance.lettuce import common
-
-
-def webi_exec_commonconf():
-    common.open_url('commonconf')
