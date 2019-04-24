d51ba3df64669d50c3788e11e88c6f428f42a427
Francois Blackburn <fblackburn@wazo.community>
2019-04-24 17:15:07 -0400

    readd deleted debug file

---
+++ xivo_acceptance/lettuce/debug.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+import requests
+
+loggers = {
+    'acceptance': logging.getLogger('acceptance'),
+    'linphone': logging.getLogger('linphone'),
+}
+
+
+def setup_logging(config):
+    file_handler = logging.FileHandler(config['log_file'])
+
+    for name, logger in loggers.iteritems():
+        logger.setLevel(logging.INFO)
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
+        logger.addHandler(file_handler)
+        if config['debug'].get(name):
+            logger.setLevel(logging.DEBUG)
+
+    requests.packages.urllib3.disable_warnings()
+
+
+def logcall(func):
+    def decorated(*args, **kwargs):
+        loggers['acceptance'].debug("calling %s(%s, %s)", func.__name__, args, kwargs)
+        func(*args, **kwargs)
+    return decorated
