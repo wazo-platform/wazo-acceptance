0f5d808eaa9c6215a8e0b70f7800e3a57f067ee8
Francois Blackburn <fblackburn@wazo.community>
2019-05-02 15:58:56 -0400

    move steps and environment.py to root features

--- xivo_acceptance/steps/__init__.py
+++ xivo_acceptance/steps/__init__.py
@@ -2,7 +2,6 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from xivo_acceptance import environment
 from .auth_steps import *
 from .files_steps import *
 from .setupd_steps import *
