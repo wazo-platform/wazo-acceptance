2ebbf31a9388acd5ec65847fa9045eb925c40314
Alexandre Fournier <afournier@wazo.io>
2019-06-05 14:12:16 -0400

    steps: fix backup steps

--- wazo_acceptance/steps/backup.py
+++ wazo_acceptance/steps/backup.py
@@ -2,28 +2,23 @@
 # Copyright 2013-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from behave import given, when, then
+from behave import given, when
 from .. import assets, auth
 
 
 @given('the asset file "{assetfile}" is copied on the server into "{serverpath}"')
-def given_the_file_is_copied_on_the_server_into_group2(context, assetfile, serverpath):
-    assets.copy_asset_to_server(assetfile, serverpath)
+def given_the_file_is_copied_on_the_server(context, assetfile, serverpath):
+    assets.copy_asset_to_server(context, assetfile, serverpath)
 
 
 @when('I execute database backup command')
 def when_i_execute_database_backup_command(context):
     command = 'bash /tmp/xivo-backup-manager backup db'
-    context.ssh_client.call([command])
-
-
-@then('executing "{command}" should complete without errors')
-def when_i_execute_without_error(context, command):
-    context.ssh_client.call([command])
+    context.ssh_client.check_call([command])
 
 
 @when('I execute database restore command')
 def when_i_execute_database_restore_command(context):
     command = 'bash /tmp/xivo-backup-manager restore db'
-    context.ssh_client.call([command])
-    auth.renew_auth_token()
+    context.ssh_client.check_call([command])
+    auth.renew_auth_token(context)
