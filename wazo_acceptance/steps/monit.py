5508d3cd312e3e9ab453efa31c079678291f128f
Francois Blackburn <fblackburn@wazo.community>
2019-07-12 08:47:58 -0400

    fix styling issues

--- wazo_acceptance/steps/monit.py
+++ wazo_acceptance/steps/monit.py
@@ -6,15 +6,19 @@ from xivo_test_helpers import until
 from behave import then
 
 
-@then(u'monit monitors the service "{process_name}"')
+@then('monit monitors the service "{process_name}"')
 def then_monit_monitors_the_service_process_name(context, process_name):
-    until.true(context.helpers.monit.process_monitored, process_name,
-               timeout=10,
-               message='Monit did not monitor process {}'.format(process_name))
+    until.true(
+        context.helpers.monit.process_monitored, process_name,
+        timeout=10,
+        message='Monit did not monitor process {}'.format(process_name),
+    )
 
 
-@then(u'monit does not monitor the service "{process_name}"')
+@then('monit does not monitor the service "{process_name}"')
 def then_monit_does_not_monitor_the_service_process_name(context, process_name):
-    until.false(context.helpers.monit.process_monitored, process_name,
-                timeout=10,
-                message='Monit is still monitoring process {}'.format(process_name))
+    until.false(
+        context.helpers.monit.process_monitored, process_name,
+        timeout=10,
+        message='Monit is still monitoring process {}'.format(process_name),
+    )
