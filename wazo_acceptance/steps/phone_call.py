b3566ad7bb930086304ed068845796d2e1a8233f
Francois Blackburn <fblackburn@wazo.community>
2019-07-16 11:00:26 -0400

    fix typo

--- wazo_acceptance/steps/phone_call.py
+++ wazo_acceptance/steps/phone_call.py
@@ -52,7 +52,7 @@ def step_a_calls_exten_and_waits_for_x_seconds(context, tracking_id, exten, time
 @when('I wait "{seconds}" seconds for the call to be forwarded')
 @when('I wait "{seconds}" seconds for the end of ringing time')
 @when('I wait "{seconds}" seconds for the timeout to not expire')
-def when_given_i_wait_n_seconds(context, seconds):
+def when_i_wait_n_seconds(context, seconds):
     _sleep(seconds)
 
 
