807e412d8fc8ed59d7482816b5257e54fe86198d
Pascal Cadotte Michaud <pcm@wazo.io>
2019-08-06 08:56:28 -0400

    auth: test that a user can change its own password

---
+++ features/daily/user_password.feature
@@ -0,0 +1,8 @@
+Feature: User password
+
+  Scenario: Change password using a user token
+    Given there are authentication users with info
+    | firstname | username | password |
+    | Greg      | greg     | 123test  |
+    When "greg" changes its password from "123test" to "foobar"
+    Then I can get a user token with username "greg" and password "foobar"
--- wazo_acceptance/steps/auth.py
+++ wazo_acceptance/steps/auth.py
@@ -2,17 +2,47 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that, is_not, none
-from behave import then
+from behave import (
+    then,
+    when,
+)
 
 from wazo_auth_client import Client as AuthClient
 
 
 @then('I can create an admin token')
 def then_i_can_create_a_token(context):
+    token = _create_token(context, 'root', 'wazosecret')
+    assert_that(token['token'], is_not(none()))
+
+
+@then('I can get a user token with username "{username}" and password "{password}"')
+def then_i_can_get_a_user_token(context, username, password):
+    token = _create_token(context, username, password)
+    assert_that(token['token'], is_not(none()))
+
+
+@when('"{username}" changes its password from "{old_password}" to "{new_password}"')
+def step_impl(context, username, old_password, new_password):
+    auth = AuthClient(
+        username=username,
+        password=old_password,
+        **context.wazo_config['auth']
+    )
+    result = auth.token.new()
+    auth.set_token(result['token'])
+
+    auth.users.change_password(
+        result['metadata']['xivo_user_uuid'],
+        old_password=old_password,
+        new_password=new_password,
+    )
+
+
+def _create_token(context, username, password):
     auth = AuthClient(
         username='root',
         password='wazosecret',
         **context.wazo_config['auth']
     )
-    token = auth.token.new()
-    assert_that(token['token'], is_not(none()))
+    return auth.token.new()
