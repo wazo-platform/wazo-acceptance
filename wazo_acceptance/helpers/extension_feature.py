62e63293e279a280fbfb239915c09beee7525211
Alexandre Fournier <afournier@wazo.io>
2019-10-23 15:39:26 -0400

    helpers: simplify extension_feature
    reason: it was doing two requests.

--- wazo_acceptance/helpers/extension_feature.py
+++ wazo_acceptance/helpers/extension_feature.py
@@ -8,28 +8,32 @@ class ExtensionFeature:
         self._context = context
         self._confd_client = context.confd_client
 
-    def get_by(self, **kwargs):
-        user = self.find_by(**kwargs)
+    def _get_by(self, **kwargs):
+        user = self._find_by(**kwargs)
         if not user:
             raise Exception('ExtensionFeature not found: {}'.format(kwargs))
         return user
 
-    def find_by(self, **kwargs):
+    def _find_by(self, **kwargs):
         extensions_features = self._confd_client.extensions_features.list(**kwargs)['items']
         for extension in extensions_features:
             return extension
 
-    def update(self, extension_feature):
-        old_feature = self._confd_client.extensions_features.get(extension_feature['id'])
+    def _update(self, body):
+        old_feature = self._get_by(search=body['feature'])
+
+        new_feature = old_feature.copy()
+        new_feature.update(body)
 
         with self._context.helpers.bus.wait_for_asterisk_reload(dialplan=True):
-            self._confd_client.extensions_features.update(extension_feature)
+            self._confd_client.extensions_features.update(new_feature)
 
         self._context.add_cleanup(self._confd_client.extensions_features.update, old_feature)
 
-    def enable(self, feature, extension=None):
-        feature = self.get_by(search=feature)
-        feature['enabled'] = True
-        if extension:
-            feature['exten'] = '_{}.'.format(extension)
-        self.update(feature)
+    def enable(self, feature_name, extension):
+        feature = {
+            'feature': feature_name,
+            'enabled': True,
+            'exten': '_{}.'.format(extension),
+        }
+        self._update(feature)
