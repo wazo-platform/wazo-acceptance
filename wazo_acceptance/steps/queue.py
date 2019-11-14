e6c40728c6e92347d52bb1184bb430ce815aef70
Francois Blackburn <fblackburn@wazo.community>
2019-11-14 16:57:35 -0500

    fix logic for queue create step when no agents

--- wazo_acceptance/steps/queue.py
+++ wazo_acceptance/steps/queue.py
@@ -38,6 +38,7 @@ def when_i_create_the_following_queues(context):
         extension = context.helpers.extension.create(body)
         context.helpers.queue.add_extension(queue, extension['id'])
 
-        for agent_number in row.get('agents', '').split(','):
-            agent = context.helpers.agent.get_by(number=agent_number)
-            context.helpers.queue.add_agent_member(queue, agent['id'])
+        if row.get('agents'):
+            for agent_number in row['agents'].split(','):
+                agent = context.helpers.agent.get_by(number=agent_number)
+                context.helpers.queue.add_agent_member(queue, agent['id'])
