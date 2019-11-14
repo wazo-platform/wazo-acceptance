ecb5e9d98e7346e4ad51e73372ba0657b5eaec9a
Francois Blackburn <fblackburn@wazo.community>
2019-11-14 14:35:26 -0500

    queue: avoid implementing unnecessary option

--- wazo_acceptance/steps/queue.py
+++ wazo_acceptance/steps/queue.py
@@ -8,25 +8,16 @@ from behave import when
 def when_i_create_the_following_queues(context):
     context.table.require_columns(['name', 'exten', 'context'])
     for row in context.table:
-        body = {
-            'name': row['name'].lower(),
-            'label': row['display_name'],
-            'options': []
-        }
-        if 'maxlen' in row:
-            body['options'].append(['maxlen', row['maxlen']])
-        if 'joinempty' in row:
-            body['options'].append(['joinempty', row['joinempty']])
-        if 'leavewhenempty' in row:
-            body['options'].append(['leavewhenempty', row['leavewhenempty']])
-        if 'ringing_time' in row:
-            body['timeout'] = row['ringing_time']
-        if 'wrapuptime' in row:
-            body['options'].append(['wrapuptime', row['wrapuptime']])
-        if 'reachability_timeout' in row:
-            body['options'].append(['timeout', row['reachability_timeout']])
-        if 'ring_strategy' in row:
-            body['options'].append(['strategy', row['ring_strategy']])
+        body = {'name': row['name'], 'label': row.get('label')}
+        queue = context.helpers.queue.create(body)
+
+        body = {'exten': row['exten'], 'context': row['context']}
+        extension = context.helpers.extension.create(body)
+        context.helpers.queue.add_extension(queue, extension['id'])
+
+        for agent_number in row.get('agents', '').split(','):
+            agent = context.helpers.agent.get_by(number=agent_number)
+            context.helpers.queue.add_agent_member(queue, agent['id'])
 
         queue = context.helpers.queue.create(body)
 
