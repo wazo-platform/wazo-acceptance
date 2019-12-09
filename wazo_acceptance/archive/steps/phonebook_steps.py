29deb6e10ed4ec3718008913d5fc895ff6cb6ab6
Francois Blackburn <fblackburn@wazo.community>
2019-12-09 07:17:32 -0500

    remove archived code associated to phonebook test

--- wazo_acceptance/archive/steps/phonebook_steps.py
+++ wazo_acceptance/archive/steps/phonebook_steps.py
@@ -3,10 +3,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from lettuce import step, world
-from hamcrest import assert_that, has_items
 
 from xivo_acceptance.action.dird import phonebook as phonebook_action_dird
-from xivo_acceptance.lettuce.aastra import AastraPhonebookBrowser
 
 
 @step(u'Given there are entries in the phonebook "([^"]*)"')
@@ -15,23 +13,3 @@ def given_there_are_entries_in_the_phonebook_1(step, phonebook_name):
     for entry in step.hashes:
         phonebook_action_dird.remove_entry_if_exists(entry, phonebook_name, entity)
         phonebook_action_dird.create_entry(entry, phonebook_name, entity)
-
-
-@step(u'When I search the phonebook for "([^"]*)" on my Aastra "([^"]*)"$')
-def when_i_search_the_phonebook_on_my_aastra(step, term, mac_address):
-    phonebook_browser = AastraPhonebookBrowser(mac_address)
-    phonebook_browser.use_provd_lookup_url()
-    world.phone_results = phonebook_browser.search(term)
-
-
-@step(u'Then I see the following results on the phone')
-def then_i_see_the_following_results_on_the_phone(step):
-    expected_results = step.hashes
-    results = _extract_results(step.keys, world.phone_results)
-
-    assert_that(results, has_items(*expected_results))
-
-
-def _extract_results(keys, phone_results):
-    results = [dict((key, phone_result[key]) for key in keys) for phone_result in phone_results]
-    return results
