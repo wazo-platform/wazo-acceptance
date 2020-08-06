dfbbcba981df5cf88e315186c4f627a3a9ad78d4
Francois Blackburn <fblackburn@wazo.community>
2020-08-06 13:47:09 -0400

    small refactor provd helper

--- wazo_acceptance/helpers/provd.py
+++ wazo_acceptance/helpers/provd.py
@@ -16,20 +16,21 @@ class Provd:
 
     def __init__(self, context):
         self._context = context
+        self._provd_client = context.provd_client
 
     def _operation_successful(self, operation_ressource):
         operation_ressource.update()
         assert_that(operation_ressource.state, is_(operation.OIP_SUCCESS))
 
     def update_plugin_list(self, url):
-        self._context.provd_client.params.update('plugin_server', url)
-        with self._context.provd_client.plugins.update() as operation_progress:
+        self._provd_client.params.update('plugin_server', url)
+        with self._provd_client.plugins.update() as operation_progress:
             until.assert_(
                 self._operation_successful, operation_progress, tries=20, interval=0.5
             )
 
     def get_latest_plugin_name(self, plugin_name):
-        all_plugins = self._context.provd_client.plugins.list_installable()['pkgs']
+        all_plugins = self._provd_client.plugins.list_installable()['pkgs']
         matching_plugins = [plugin for plugin in all_plugins if plugin_name in plugin]
         if not matching_plugins:
             raise Exception('no matching plugins for {}'.format(plugin_name))
@@ -37,12 +38,12 @@ class Provd:
 
     def install_latest_plugin(self, plugin):
         latest_plugin = self.get_latest_plugin_name(plugin)
-        plugins_installed = self._context.provd_client.plugins.list_installed()['pkgs']
+        plugins_installed = self._provd_client.plugins.list_installed()['pkgs']
         if latest_plugin not in plugins_installed:
             self.install_plugin(latest_plugin)
 
     def install_plugin(self, plugin_name):
-        with self._context.provd_client.plugins.install(plugin_name) as operation_progress:
+        with self._provd_client.plugins.install(plugin_name) as operation_progress:
             until.assert_(
                 self._operation_successful, operation_progress, tries=20, interval=0.5
             )
@@ -54,22 +55,19 @@ class Provd:
             self._context.add_cleanup(self.delete_device_with_mac, mac)
 
     def delete_device(self, device_id):
-        device_manager = self._context.provd_client.devices
-        config_manager = self._context.provd_client.configs
-
-        devices = device_manager.list({'id': device_id})['devices']
+        devices = self._provd_client.devices.list({'id': device_id})['devices']
         device = devices[0] if devices else None
         if not device:
             return
 
         config_id = device.get('config', device['id'])
-        configs = config_manager.list({'id': config_id})['configs']
+        configs = self._provd_client.configs.list({'id': config_id})['configs']
         config = configs[0] if configs else None
 
-        device_manager.delete(device['id'])
+        self._provd_client.devices.delete(device['id'])
         if config:
             try:
-                config_manager.delete(config['id'])
+                self._provd_client.configs.delete(config['id'])
             except ProvdError:
                 pass
 
@@ -79,6 +77,5 @@ class Provd:
             self.delete_device(device['id'])
 
     def _find_by(self, key, value):
-        device_manager = self._context.provd_client.devices
-        devices = device_manager.list({key: value})['devices']
+        devices = self._provd_client.devices.list({key: value})['devices']
         return devices[0] if devices else None
