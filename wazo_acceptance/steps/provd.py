c19dc36a9e97e439da80d5628378758b1a31f661
Sebastien Duthil <sduthil@wazo.community>
2020-09-24 10:28:53 -0400

    provd: factorize config assertion

--- wazo_acceptance/steps/provd.py
+++ wazo_acceptance/steps/provd.py
@@ -19,15 +19,11 @@ def given_the_latest_plugin_is_installed(context, plugin):
 def provd_config_has_the_following_values(context, config_id, instance):
     instance_context = getattr(context.instances, instance)
     config = instance_context.helpers.provd.get_config(config_id)
+    slave_host = instance_context.wazo_config.get('slave_host') or '<unknown>'
     for row in context.table:
         expected = row.as_dict()
-        for key, value in expected.items():
-            if '{{ slave_voip_ip_address }}' in value:
-                rendered_value = value.replace(
-                    '{{ slave_voip_ip_address }}',
-                    instance_context.wazo_config.get('slave_host') or '<unknown>',
-                )
-                assert_that(config.get(key, ''), equal_to(rendered_value))
+        expected = _render_expected_config(expected, slave_host)
+        assert_that(config, has_entries(expected))
 
 
 @then('the provd offline config "{config_id}" has the following values on "{instance}"')
@@ -40,6 +36,11 @@ def provd_offline_config_has_the_following_values(context, config_id, instance):
     slave_host = instance_context.wazo_config.get('slave_host') or '<unknown>'
     for row in context.table:
         expected = row.as_dict()
-        expected = {key: value.replace('{{ slave_voip_ip_address }}', slave_host) for key, value in expected.items()}
-        expected = {key: (value or None) for key, value in expected.items()}
+        expected = _render_expected_config(expected, slave_host)
         assert_that(config, has_entries(expected))
+
+
+def _render_expected_config(expected, slave_host):
+    rendered = {key: value.replace('{{ slave_voip_ip_address }}', slave_host) for key, value in expected.items()}
+    rendered = {key: (value or None) for key, value in expected.items()}
+    return rendered
