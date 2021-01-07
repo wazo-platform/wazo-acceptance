9779dba654cb704af102a6067e6716c20482dce1
Francois Blackburn <fblackburn@wazo.community>
2021-01-07 10:26:33 -0500

    extension_feature: remove indirection logic
    reason: this logic was used when the interface was tested. But now only
    API are tested and we should use API options name

--- wazo_acceptance/steps/extension_feature.py
+++ wazo_acceptance/steps/extension_feature.py
@@ -1,17 +1,14 @@
-# Copyright 2013-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from behave import given
 
-_DESCRIPTION_TO_FEATURE = {
-    'forwarding on no-answer': 'fwdrna',
-    'forwarding on busy': 'fwdbusy',
-    'unconditional forwarding': 'fwdunc',
-    'do not disturb': 'enablednd',
-    'incoming call filtering': 'incallfilter',
-    'call recording': 'callrecord',
-}
+
+@given('the "{extension_name}" extension is enabled')
+def given_the_extension_is_enabled(context, extension_name):
+    context.helpers.extension_feature.enable(extension_name)
+
 
 @given('the "{extension_name}" extension is disabled')
 def given_the_extension_is_disabled(context, extension_name):
-    context.helpers.extension_feature.disable(_DESCRIPTION_TO_FEATURE[extension_name])
+    context.helpers.extension_feature.disable(extension_name)
