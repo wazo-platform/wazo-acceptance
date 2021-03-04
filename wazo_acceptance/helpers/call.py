8522c04bb5e3c52754a1eceb32cf5b4734f3ea79
Francois Blackburn <fblackburn@wazo.community>
2021-03-04 15:35:32 -0500

    call: allow to filter on None value
    reason: used by switchboard step which require to filter on
    user_uuid=None

--- wazo_acceptance/helpers/call.py
+++ wazo_acceptance/helpers/call.py
@@ -1,6 +1,8 @@
 # Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+_UNDEFINED = object()
+
 
 class Call:
 
@@ -21,15 +23,18 @@ class Call:
         self._calld_client.calls.start_record(call_id)
 
     def _find_by(self, **kwargs):
-        user_uuid = kwargs.pop('user_uuid', None)
-        caller_id_number = kwargs.pop('caller_id_number', None)
-        caller_id_name = kwargs.pop('caller_id_name', None)
+        user_uuid = kwargs.pop('user_uuid', _UNDEFINED)
+        caller_id_number = kwargs.pop('caller_id_number', _UNDEFINED)
+        caller_id_name = kwargs.pop('caller_id_name', _UNDEFINED)
         calls = self._calld_client.calls.list_calls(**kwargs)['items']
         for call in calls:
-            if user_uuid and user_uuid != call['user_uuid']:
-                continue
-            if caller_id_number and caller_id_number != call['caller_id_number']:
-                continue
-            if caller_id_name and caller_id_name != call['caller_id_name']:
-                continue
+            if user_uuid is not _UNDEFINED:
+                if user_uuid != call['user_uuid']:
+                    continue
+            if caller_id_number is not _UNDEFINED:
+                if caller_id_number != call['caller_id_number']:
+                    continue
+            if caller_id_name is not _UNDEFINED:
+                if caller_id_name != call['caller_id_name']:
+                    continue
             return call
