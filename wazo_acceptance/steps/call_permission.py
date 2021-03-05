003b41211a65ece33b21c5153f73e636c1be74f9
Alexandre Fournier <afournier@wazo.io>
2021-03-05 14:10:00 -0500

    call_permission: add groups tests

--- wazo_acceptance/steps/call_permission.py
+++ wazo_acceptance/steps/call_permission.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from behave import given
@@ -17,3 +17,8 @@ def given_there_are_call_permissions_with_infos(context):
             for name in body.get('users').split(','):
                 user = context.helpers.confd_user.get_by(search=name)
                 context.confd_client.call_permissions(call_permission).add_user(user)
+
+        if body.get('groups'):
+            for name in body.get('groups').split(','):
+                group = context.helpers.confd_group.get_by(search=name)
+                context.confd_client.groups(group).add_call_permission(call_permission)
