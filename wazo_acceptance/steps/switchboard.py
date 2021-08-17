6259f657b97f722883107e13e564b8b58cc6f4c1
Sebastien Duthil <sduthil@wazo.community>
2021-08-17 16:15:15 -0400

    switchboards: assert caller id number AND name
    Why:
    
    * Makes steps more readable with callerid name

--- wazo_acceptance/steps/switchboard.py
+++ wazo_acceptance/steps/switchboard.py
@@ -9,26 +9,40 @@ def given_there_are_switchboards_with_infos(context):
     context.table.require_columns(['name'])
     for row in context.table:
         body = row.as_dict()
+        timeout = body.pop('timeout', None)
+        if timeout:
+            body['timeout'] = int(timeout)
         switchboard = context.helpers.switchboard.create(body)
         members = body['members'].split(',') if body.get('members') else []
         users = [context.helpers.confd_user.get_by(fullname=name) for name in members]
         context.confd_client.switchboards(switchboard).update_user_members(users)
+
+        if body.get('noanswer_destination'):
+            type_, name = body['noanswer_destination'].split(':')
+            fallbacks_body = {'noanswer_destination': {'type': type_}}
+            if type_ == 'hangup':
+                fallbacks_body['noanswer_destination']['cause'] = 'normal'
+            else:
+                raise NotImplementedError('Destination not implemented: {}'.format(type_))
+            context.confd_client.switchboards(switchboard).update_fallbacks(fallbacks_body)
 
 
 @then('switchboard "{name}" has "{caller_name}" in queued calls')
 def then_switchboard_has_callerid_in_queued_calls(context, name, caller_name):
     switchboard = context.helpers.switchboard.get_by(name=name)
     calls = context.calld_client.switchboards.list_queued_calls(switchboard['uuid'])
-    caller_names = [call['caller_id_number'] for call in calls['items']]
-    assert caller_name in caller_names
+    caller_numbers = [call['caller_id_number'] for call in calls['items']]
+    caller_names = [call['caller_id_name'] for call in calls['items']]
+    assert caller_name in (caller_names + caller_numbers)
 
 
 @then('switchboard "{name}" has "{caller_name}" in held calls')
 def then_switchboard_has_callerid_in_held_calls(context, name, caller_name):
     switchboard = context.helpers.switchboard.get_by(name=name)
     calls = context.calld_client.switchboards.list_held_calls(switchboard['uuid'])
-    caller_names = [call['caller_id_number'] for call in calls['items']]
-    assert caller_name in caller_names
+    caller_numbers = [call['caller_id_number'] for call in calls['items']]
+    caller_names = [call['caller_id_name'] for call in calls['items']]
+    assert caller_name in (caller_names + caller_numbers)
 
 
 @when('"{firstname} {lastname}" answer queued call "{caller_name}" from switchboard "{name}"')
