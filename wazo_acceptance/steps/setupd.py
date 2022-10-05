e7a7b926d162938cc2595308be7f4c178c80f1a3
nissou91 <achohra@wazo.io>
2022-10-05 12:01:41 -0400

    ensure appropriate sound files are installed

--- wazo_acceptance/steps/setupd.py
+++ wazo_acceptance/steps/setupd.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from behave import when
+from hamcrest import assert_that, equal_to
 
 
 @when('I pass the setup')
@@ -16,6 +17,16 @@ def when_i_pass_the_setup(context):
 
 @when('I pass the setup with "{setup_language}" as default language')
 def when_i_pass_the_setup_with_specific_language(context, setup_language):
+    sound_file_language = setup_language.lower().replace('_','-')
+    sound_file_name = f'asterisk-sounds-wav-{sound_file_language}'
+    assert_that(
+        context.remote_sysutils.send_command(['apt-get', 'update'], check=True),
+        equal_to(True),
+    )
+    assert_that(
+        context.remote_sysutils.send_command(['apt-get', '-y', 'install', sound_file_name], check=True),
+        equal_to(True),
+    )
     body = {
         'engine_internal_address': '192.168.32.240',
         'engine_language': setup_language,
