23e67bb421d6a9b43df0444c3c5f07edf18c7dbf
Alexandre Fournier <afournier@wazo.io>
2023-10-30 15:32:19 -0400

    provisioning: add secure key test

--- wazo_acceptance/steps/device.py
+++ wazo_acceptance/steps/device.py
@@ -1,8 +1,8 @@
 # Copyright 2013-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from behave import given, step, when, then
 import requests
+from behave import given, step, then, when
 
 
 @given('there are devices with infos')
@@ -24,6 +24,17 @@ def when_the_following_devices_are_created_via_http_requets_to_the_provisioning_
         body['user_agent'] = body.pop('user-agent')
         context.helpers.provd.create_device_via_http_request(**body)
 
+@then('the following provisioning files are available over HTTPS using secure key "{secure_key}"')
+def then_the_following_provisioning_files_are_available_over_https_with_url_key(context, secure_key):
+    for row in context.table:
+        file_ = row.as_dict()
+        url = f"https://{context.wazo_config['wazo_host']}/device/provisioning/{secure_key}/{file_['path']}"
+        headers = {
+            'User-Agent': file_['user-agent']
+        }
+        response = requests.get(url, headers=headers, verify=False)
+        response.raise_for_status()
+        assert file_['expected_content'] in response.text
 
 @when('I synchronize the device with mac "{mac}"')
 def when_i_synchronize_the_device_with_mac(context, mac):
