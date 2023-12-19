af2b3bc87d997cd395ea21eeebb3facfd55ad962
Francois Blackburn <fblackburn@wazo.community>
2023-12-19 14:43:00 -0500

    add tox linter with pre-commit to run flake8
    why: to replace pylint

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_acceptance.environment import *
+from wazo_acceptance.environment import *  # noqa
