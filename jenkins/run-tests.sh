b65cfae706f8441368108d663f65bd08bd26c60c
Sebastien Duthil <sduthil@wazo.community>
2024-06-18 09:54:28 -0400

    jenkins: add declarative pipeline

---
+++ jenkins/run-tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+mkdir -p /var/lib/jenkins/.wazo-acceptance
+mkdir -p ${WORKSPACE}/junit/{pre_daily,daily}
+
+cat > /var/lib/jenkins/.wazo-acceptance/config.yml <<EOF
+instances:
+  default:
+    # When running on acceptance-ci worker
+    wazo_host: 10.27.5.35
+    # When running directly on jenkins
+    # wazo_host: daily-wazo-rolling-dev.lan.wazo.io
+    # nat:
+    #   local_net: 10.27.0.0/16
+    #   external_ip: 172.16.43.37
+debug:
+  global: True
+
+  acceptance: True
+  linphone: True
+  wazo_test_helpers: True
+EOF
+
+set -xe -o pipefail
+
+VENV=wazo-acceptance-venv
+python3.9 -m venv $VENV
+source $VENV/bin/activate
+pip install wheel
+
+pip install -r requirements.txt
+pip install coverage
+python setup.py install
+docker pull wazoplatform/wazo-linphone
+
+coverage run --source=wazo_acceptance $(which behave) features/pre_daily --verbose --junit --junit-directory=junit/pre_daily 2>&1 | tee --append acceptance.log
+coverage run -a --source=wazo_acceptance $(which wazo-acceptance) -v -p 2>&1
+coverage run -a --source=wazo_acceptance $(which behave) --no-capture --no-color features/daily --verbose --junit --junit-directory=junit/daily 2>&1 | tee --append acceptance.log
+
+coverage xml
